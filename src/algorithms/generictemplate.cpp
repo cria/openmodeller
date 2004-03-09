7bde9bcbe4ed27be83e3a40c39823bc2517b2ce3
Tim Sutton <tim@kartoza.com>
2004-03-09 18:19:48 +0000

    This is a template that can be used to create new algorithms. It contains fairly detailed explanation of the parameters and methods that will hopefully make it easy to create new algorithms.

---
+++ src/algorithms/generictemplate.cpp
@@ -0,0 +1,115 @@
+//
+// GenericTemplate
+//
+// Description:
+//
+//
+// Author: CRIA <t.sutton@reading.ac.uk>, (C) 2004
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "generictemplate.hh"
+
+
+/** Constructor for GenericTemplate
+   * 
+   * @param Sampler is class that will fetch environment variable values at each occurrence / locality
+   */
+GenericTemplate::GenericTemplate( Sampler *samp )
+{
+}
+
+/** This is the descructor for the GenericTemplate class */
+GenericTemplate::~GenericTemplate()
+{
+
+
+}
+
+
+
+//
+// Methods used to build the model
+//
+
+/** Metadata field returning the name of this algorithm.
+  * @note This method is inherited from the Algorithm class 
+  * @return a character array containing the name of the algorithm
+  */
+char * GenericTemplate::name()
+{
+    return "GenericTemplate";
+} //replace this name!
+
+/** This method is used when you want to ensure that all variables in all
+  * environmental layers are scaled to the same value range. 
+  * @note This method is inherited from the Algorithm class
+  * @param Scalar pointer min that the post normalised will be fitted to
+  * @param Scalar pointer max that the post normalised will be fitted to
+  * @return 0 if no normalisation is needed
+  */
+int GenericTemplate::needNormalization( Scalar *min, Scalar *max )
+{
+
+}
+
+
+/** Initialise the model specifying a threshold / cutoff point.
+  * This is optional (model dependent).
+  * @note This method is inherited from the Algorithm class
+  * @param 
+  * @return 0 on error
+  */
+int GenericTemplate::initialize( int ncycle )
+{
+
+}
+
+
+/** Start model execution (build the model).
+  * @note This method is inherited from the Algorithm class
+  * @return 0 on error 
+  */
+int GenericTemplate::iterate()
+{
+
+}
+
+
+/** Use this method to find out if the model has completed (e.g. convergence
+  * point has been met. 
+  * @note This method is inherited from the Algorithm class
+  * @return     
+  * @return Implementation specific but usually 1 for completion.
+  */
+int GenericTemplate::done()
+{
+
+}
+
+
+//
+// Methods used to project the model
+//
+
+
+/** This method is used when projecting the model.
+  * @note This method is inherited from the Algorithm class
+  * @return     
+  * @param Scalar *x a pointer to a vector of openModeller Scalar type (currently double). The vector should contain values looked up on the environmental variable layers into which the mode is being projected. */
+Scalar GenericTemplate::getValue( Scalar *x )
+{
+
+}
+
+/** Returns a value that represents the convergence of the algorithm
+  * expressed as a number between 0 and 1 where 0 represents model
+  * completion. 
+  * @return 
+  * @param Scalar *val 
+*/
+int GenericTemplate::getConvergence( Scalar *val )
+{
+
+}
---
+++ src/algorithms/generictemplate.hh
@@ -0,0 +1,126 @@
+/**
+ * This is a generic template intended to be used as the basis for
+ * creating new algorithms.
+ * 
+ * @file
+ * @author Tim Sutton (t.sutton@reading.ac.uk)
+ * @date   2003-09-12
+ * $Id$
+ * 
+ * LICENSE INFORMATION 
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+ 
+#ifndef GENERIC_TEMPLATE_H
+#define GENERIC_TEMPLATE_H
+
+#include <algorithm.hh>
+
+/**
+This is a simple template for creating new algorithms.  Note it does not actually run a model - rather it demonstrates how to do a new algorithm implementation.
+
+@author Tim Sutton
+*/
+class GenericTemplate : public Algorithm
+{
+public:
+
+    /** Constructor for GenericTemplate
+      * 
+      * @param Sampler is class that will fetch environment variable 
+      * values at each occurrence / locality
+      */
+    GenericTemplate( Sampler *samp );
+    /** This is the descructor for the GenericTemplate class */
+    ~GenericTemplate();
+    
+    public:
+
+  //
+  // Methods used to build the model
+  //
+  
+  /** Metadata field returning the name of this algorithm.
+    * @note This method is inherited from the Algorithm class 
+    * @return a character array containing the name of the algorithm
+    */
+  char *name()    { return "GenericTemplate"; } //replace this name!
+  
+  /** This method is used when you want to ensure that all variables in all
+    * environmental layers are scaled to the same value range. 
+    * @note This method is inherited from the Algorithm class
+    * @param Scalar pointer min that the post normalised will be fitted to
+    * @param Scalar pointer max that the post normalised will be fitted to
+    * @return 0 if no normalisation is needed
+    */
+  int needNormalization( Scalar *min, Scalar *max );
+  
+  /** Initialise the model specifying a threshold / cutoff point.
+    * This is optional (model dependent).
+    * @note This method is inherited from the Algorithm class
+    * @return 0 on error
+    */
+  int initialize( int ncycle );
+  
+  /** Start model execution (build the model).     
+    * @note This method is inherited from the Algorithm class
+    * @return 0 on error 
+    */
+  int   iterate();
+  
+  /** Use this method to find out if the model has completed (e.g. convergence
+    * point has been met. 
+    * @note This method is inherited from the Algorithm class
+    * @return     
+    * @return Implementation specific but usually 1 for completion.
+    */
+  int   done();
+
+  //
+  // Methods used to project the model
+  //
+  
+  
+  /** This method is used when projecting the model.  
+    * @note This method is inherited from the Algorithm class
+    * @return     
+    * @param Scalar *x a pointer to a vector of openModeller Scalar type (currently double). The vector should contain values looked up on the environmental variable layers into which the mode is being projected. */
+  Scalar getValue( Scalar *x );
+  
+  /** Returns a value that represents the convergence of the algorithm
+    * expressed as a number between 0 and 1 where 0 represents model
+    * completion. 
+    * @return 
+    * @param Scalar *val 
+  */
+  int    getConvergence( Scalar *val );
+
+
+private:
+  
+  /** This member variable is used to indicate whether the model 
+    * building process has completed yet. */
+  int    f_done;
+  
+  Scalar *f_avg;  ///< Average related to occurrence points.
+  int     f_dim;  ///< Dimension of environmental space.
+
+};
+
+#endif
