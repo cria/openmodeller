c827c408dc493ca8e7c47a1b9cb259f53d650f78
mesm <mesm@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-03-13 04:08:09 +0000

    *** empty log message ***

--- trunk/openmodeller/src/algorithms/bioclim.cpp
+++ trunk/openmodeller/src/algorithms/bioclim.cpp
@@ -44,13 +44,13 @@
 BioclimModel::BioclimModel( Sampler *samp, Scalar cutoff )
   : Algorithm( samp )
 {
-  f_done   = 0;
-  f_cutoff = cutoff;
-  f_dim    = samp->dim() - 1;
-  f_max    = new Scalar[f_dim];
-  f_min    = new Scalar[f_dim];
-  f_avg    = new Scalar[f_dim];
-  f_range  = new Scalar[f_dim];
+  _done   = 0;
+  _cutoff = cutoff;
+  _dim    = _samp->dimEnv();
+  _max    = new Scalar[_dim];
+  _min    = new Scalar[_dim];
+  _avg    = new Scalar[_dim];
+  _half_range = new Scalar[_dim];
 }
 
 
@@ -59,8 +59,8 @@ BioclimModel::BioclimModel( Sampler *samp, Scalar cutoff )
 
 BioclimModel::~BioclimModel()
 {
-  delete f_min;
-  delete f_max;
+  delete _min;
+  delete _max;
 }
 
 
@@ -69,51 +69,47 @@ BioclimModel::~BioclimModel()
 int
 BioclimModel::iterate()
 {
-  Sampler *samp = getSampler();
+  _log.info( "Reading %d-dimensional occurrence points.\n", _dim );
 
-  // Generate model from the average of given points.
-  int npnt = samp->numOccurrences();
-
-  _log.info( "Reading %d-dimensional occurrence points.\n", f_dim );
-
-  Samples pnt( npnt, f_dim );
-  npnt = samp->getOccurrences( npnt, &pnt );
+  SampledData presence;
+  _samp->getPresence( &presence );  // Get all presence points.
+  Scalar *pnt = presence.pnt;
+  int    npnt = presence.npnt;
 
   if ( ! npnt )
-    {
-      _log.error( 1, "All occurrences are outside the mask!\n" );
-    }
+    _log.error( 1, "All occurrences are outside the mask!\n" );
 
-  _log.info( "Finding the bioclimatic envelope using %d points.\n", npnt );
+  _log.info( "Finding the bioclimatic envelope using %d points.\n",
+	     npnt );
 
   // sort values of each independent variable and apply cutoffs
-  memset( f_min,   0, f_dim * sizeof(Scalar) );
-  memset( f_max,   0, f_dim * sizeof(Scalar) );
-  memset( f_avg,   0, f_dim * sizeof(Scalar) );
-  memset( f_range, 0, f_dim * sizeof(Scalar) );
+  memset( _min, 0, _dim * sizeof(Scalar) );
+  memset( _max, 0, _dim * sizeof(Scalar) );
+  memset( _avg, 0, _dim * sizeof(Scalar) );
+  memset( _half_range, 0, _dim * sizeof(Scalar) );
 
   // temp array that will store the values of the current env variable
   // this will be used by the sort algorithm
-  Scalar * values = new Scalar[npnt];
+  Scalar *values = new Scalar[npnt];
 
   // get number of points on the borders of distribution to be removed
-  if (f_cutoff > 1.0)
-    f_cutoff = 1.0;
-  else if (f_cutoff < 0.0)
-    f_cutoff = 0.0;
+  if (_cutoff > 1.0)
+    _cutoff = 1.0;
+  else if (_cutoff < 0.0)
+    _cutoff = 0.0;
 
-  int ncutoff = (int) ceil(npnt * (1 - f_cutoff) / 2.0);
+  int ncutoff = (int) ceil(npnt * (1 - _cutoff) / 2.0);
 
   // visit each variable
   int d, i, j, k;
-  for ( d = 0; d < f_dim; d++ )
+  for ( d = 0; d < _dim; d++ )
     {
       _log.info( "Bioclimatic Envelope on dimension %d.\n", d );
 
       // get all values for this variable and store in second array
       memset( values, 0, npnt * sizeof(Scalar) );
       for ( i = 0; i < npnt; i++)
-	values[i] = pnt( i, d + 1 );
+	values[i] = pnt[ i * _dim + d ];
 
       // sort the values of this env variable
       // using bubble sort for simplicity
@@ -159,18 +155,21 @@ BioclimModel::iterate()
 
       // get cutoff values by picking the ncutoff-th and 
       // (npnt-ncutoff)-th elements
-      f_min[d] = values[ncutoff];
-      f_max[d] = values[npnt - ncutoff - 1];
+      _min[d] = values[ncutoff];
+      _max[d] = values[npnt - ncutoff - 1];
 
-      f_range[d] = f_max[d] - f_min[d];
-      f_avg[d] = (f_max[d] - f_min[d]) / 2.0;
+      _half_range[d] = (_max[d] - _min[d]) / 2.0;
+      _avg[d] = (_max[d] + _min[d]) / 2.0;
+
+      _log.debug( "%d] Min: %f, Max: %f, Avg: %f, Range: %f\n",
+		  d, _min[d], _max[d], _avg[d], _half_range[d] );
     }
 
   _log.debug( "Finished model\n" );
 
   delete values;
 
-  f_done = 1;
+  _done = 1;
   return 1;
 }
 
@@ -180,7 +179,7 @@ BioclimModel::iterate()
 int
 BioclimModel::done()
 {
-  return f_done;
+  return _done;
 }
 
 
@@ -189,23 +188,23 @@ BioclimModel::done()
 Scalar
 BioclimModel::getValue( Scalar *x )
 {
-  Scalar result;
-  int d;
+  Scalar dif;
+  Scalar result[_dim];
 
   // check whether x is inside all variable intervals
-  result = 1.0;
-  for (d = 0; d < f_dim; d++)
-    if ((x[d] < f_min[d]) || (x[d] > f_max[d]))
-      {
-	result = 0.0;
-	break;
-      }
+  for ( int d = 0; d < _dim; d++ )
+
+    if ( (x[d] < _min[d]) || (x[d] > _max[d]) )
+      return 0.0;
+
     else
       {
-	result *= Abs(x[d] - f_avg[d]) / f_range[d];
+	dif = x[d] - _avg[d];
+	result[d] = Abs(dif) / _half_range[d];
       }
 
-  return result;
+  // Inverse of the distance normalized in [0,1].
+  return 1.0 - cartesianDistance( result, _avg ) / sqrt( _dim );
 }
 
 
@@ -216,5 +215,24 @@ BioclimModel::getConvergence( Scalar *val )
 {
   *val = 1.0;
   return 1;
+}
+
+
+/**************************/
+/*** cartesian Distance ***/
+Scalar
+BioclimModel::cartesianDistance( Scalar *x, Scalar *y )
+{
+  Scalar dif;
+  Scalar dist = 0.0;
+
+  Scalar *end = x + _dim;
+  while ( x < end )
+    {
+      dif = *x++ - *y++;
+      dist += dif * dif;
+    }
+
+  return sqrt( dist );
 }
 
