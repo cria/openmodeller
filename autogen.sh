2c33460565e52a82a90c2e14703c31c677324402
jobi <jobi@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-03-25 12:06:33 +0000

    make algorithms independent libs

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo Configuring build environment for openModeller
+echo Configuring build environment for openmodeller
 aclocal  \
   && libtoolize --force --copy \
   && automake --add-missing --foreign --copy \
   && autoconf --force \
-  && echo Now running configure to configure openModeller \
-	&& ./configure
+  && echo Now running configure to configure openmodeller \
+  && ./configure
