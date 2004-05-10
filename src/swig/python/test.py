28bc04408177c85e3bcf4059906b7e03087fc8fc
scachett <scachett@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-05-10 20:41:14 +0000

    Completed example in Python using SWIG binding.

--- src/swig/python/test.py
+++ src/swig/python/test.py
@@ -1,14 +1,15 @@
 import om;
 
-ctrl = om.ControlInterface();
+mod = om.OpenModeller();
 print "Welcome to openModeller Python Binding test.\n";
-print "Using openModeller version:     ", ctrl.getVersion();
-print "Loading algorithms from:        ", ctrl.getPluginPath();
-print "Algorithms loaded:              ", ctrl.loadAlgorithms();
-print "Number of available algorithms: ", ctrl.numAvailableAlgorithms();
+print "Using openModeller version:     ", mod.getVersion();
+print "Loading algorithms from:        ", mod.getPluginPath();
+print "Algorithms loaded:              ", mod.loadAlgorithms();
+print "Number of available algorithms: ", mod.numAvailableAlgorithms();
 
-md = ctrl.algorithmMetadata("GARP v3.0.1 alpha");
-#md = ctrl.algorithmMetadata("CSM v0.1 alpha");
+#md = mod.algorithmMetadata("MinimumDistance v0.1");
+md = mod.algorithmMetadata("GARP v3.0.1 alpha");
+#md = mod.algorithmMetadata("CSM v0.1 alpha");
 
 print "Algorithm info: ";
 print "Id:             ", md.id;
@@ -19,13 +20,15 @@ print "Contact:        ", md.contact;
 
 maps = ["aspect", "dem", "slope", "dtr", "prec", "temp", "wet"];
 
-params = [["a","b"], ["c","d"], ["e","f"]];
-
-#params = om.AlgParameter();
-#params.setName("MaxGenerations");
-#params.setValue(100);
+params = [["MaxGenerations",   "50"], 
+          ["ConvergenceLimit", "0.0"],
+          ["PopulationSize",   "50"],
+          ["Resamples",        "2500"],
+          ["MutationRate",     "0.25"],
+          ["CrossoverRate",    "0.25"]];
 
 hdr = om.Header();
+#hdr = om.Header_Params(360, 180, -180, -90, 180, 90, 0, 1);
 
 hdr.xdim = 360;
 hdr.ydim = 180;
@@ -36,13 +39,21 @@ hdr.ymax = 90;
 hdr.noval = 0;
 hdr.nband = 1;
 
+wktsys = "GEOGCS[\"WGS84\", DATUM[\"WGS84\", SPHEROID[\"WGS84\", 6378137.0, 298.257223563]], PRIMEM[\"Greenwich\", 0.0], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Longitude\",EAST], AXIS[\"Latitude\",NORTH]]";
+spfile = "Strix_varia.txt";
+spname = "Strix varia";
+
+ocfile = om.OccurrencesFile(spfile, wktsys);
+ocfile.tail();
+occurr = ocfile.remove(spname);
+
 print "\nInitializing algorithm.";
-print "Setting Environment object. ", ctrl.setEnvironment(0, [], 7, maps, "mask");
-print "Setting Output format.      ", ctrl.setOutputMap("map.tif", "mask", 255);
-#print "Setting Output format.      ", ctrl.setOutputMap_Header("map.tif", hdr, 255);
-print "Setting algorithm params.   ", ctrl.setAlgorithm(md.id, 0, params);
-print "Reading occurrences.        ", 0;
-print "Running algorithm.          ", 0;
+print "Setting Environment object. ", mod.setEnvironment(0, [], 7, maps, "mask");
+print "Setting Output format.      ", mod.setOutputMap("map.tif", "mask", 255);
+#print "Setting Output format.      ", mod.setOutputMap_Header("map.tif", hdr, 255);
+print "Setting algorithm params.   ", mod.setAlgorithm(md.id, 6, params);
+print "Reading occurrences.        ", mod.setOccurrences(occurr);
+print "Running algorithm.          ", mod.run();
 
 print "Finalizing algorithm";
 
