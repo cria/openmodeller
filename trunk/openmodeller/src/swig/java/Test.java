74389f133c31c9669f412f7ab4af18b9cd6761f9
scachett <scachett@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-05-20 21:45:04 +0000

    Improved Java test class a little.

--- trunk/openmodeller/src/swig/java/Test.java
+++ trunk/openmodeller/src/swig/java/Test.java
@@ -23,6 +23,33 @@ public class Test
 	System.out.println("Author:         " + md.getAuthor());
 	System.out.println("Contact:        " + md.getContact());
 
+	String categs[] = {};
+	String maps[] = {"aspect", "dem", "slope", "dtr", "prec", "temp", "wet"};
+
+	Header hdr = new Header();
+	
+	String wktsys = "GEOGCS[\"WGS84\", DATUM[\"WGS84\", SPHEROID[\"WGS84\", 6378137.0, 298.257223563]], PRIMEM[\"Greenwich\", 0.0], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Longitude\",EAST], AXIS[\"Latitude\",NORTH]]";
+	String spfile = "Strix_varia.txt";
+	String spname = "Strix varia";
+	
+	//OccurrencesFile ocfile = new OccurrencesFile(spfile, wktsys);
+	//ocfile.tail();
+	//SWIGTYPE_p_Occurrences occurr = ocfile.remove(spname);
+	
+	System.out.println("\nInitializing algorithm.");
+	//System.out.println("Setting Environment object. " + mod.setEnvironment(0, categs, 7, maps, "mask"));
+	//System.out.println("Setting Output format.      " + mod.setOutputMap("map.tif", "mask", 255));
+	//System.out.println("Setting algorithm params.   " + mod.setAlgorithm(md.id, 6, params));
+	//System.out.println("Reading occurrences.        " + mod.setOccurrences(occurr, null));
+	//System.out.println("Running algorithm.          " + mod.run());
+
+	System.out.println("Finalizing algorithm");
+
+	System.out.println("\nThanks for using openModeller.");
+	System.out.println("Good bye!\n");
+
+
+
 	System.out.println("");
     }
 }
