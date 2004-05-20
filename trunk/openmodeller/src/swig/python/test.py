a547da704d065a0bd4853405b7f0cadccf6a2892
scachett <scachett@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-05-20 19:03:03 +0000

    Small fix to test.py. Somehow the main class name was wrong.

--- trunk/openmodeller/src/swig/python/test.py
+++ trunk/openmodeller/src/swig/python/test.py
@@ -1,6 +1,6 @@
 import om;
 
-mod = om.OpenModeller();
+mod = om.ControlInterface();
 print "Welcome to openModeller Python Binding test.\n";
 print "Using openModeller version:     ", mod.getVersion();
 print "Loading algorithms from:        ", mod.getPluginPath();
@@ -28,16 +28,6 @@ params = [["MaxGenerations",   "50"],
           ["CrossoverRate",    "0.25"]];
 
 hdr = om.Header();
-#hdr = om.Header_Params(360, 180, -180, -90, 180, 90, 0, 1);
-
-hdr.xdim = 360;
-hdr.ydim = 180;
-hdr.xmin = -180;
-hdr.ymin = -90;
-hdr.xmax = 180;
-hdr.ymax = 90;
-hdr.noval = 0;
-hdr.nband = 1;
 
 wktsys = "GEOGCS[\"WGS84\", DATUM[\"WGS84\", SPHEROID[\"WGS84\", 6378137.0, 298.257223563]], PRIMEM[\"Greenwich\", 0.0], UNIT[\"degree\", 0.017453292519943295], AXIS[\"Longitude\",EAST], AXIS[\"Latitude\",NORTH]]";
 spfile = "Strix_varia.txt";
@@ -50,7 +40,6 @@ occurr = ocfile.remove(spname);
 print "\nInitializing algorithm.";
 print "Setting Environment object. ", mod.setEnvironment(0, [], 7, maps, "mask");
 print "Setting Output format.      ", mod.setOutputMap("map.tif", "mask", 255);
-#print "Setting Output format.      ", mod.setOutputMap_Header("map.tif", hdr, 255);
 print "Setting algorithm params.   ", mod.setAlgorithm(md.id, 6, params);
 print "Reading occurrences.        ", mod.setOccurrences(occurr);
 print "Running algorithm.          ", mod.run();
