d9c834d826de9b82b3078e0271eb79c745612a22
mesm <mesm@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-05-28 21:17:09 +0000

    A different version of bioclim that calculates the probabilities inside all envelops according with the distance to the mean.

---
+++ trunk/openmodeller/src/algorithms/bioclim_distance.hh
@@ -0,0 +1,82 @@
+/**
+ * Declaration of Bioclimatic Envelope Algorithm - Nix, 1986.
+ * 
+ * @file
+ * @author Mauro Muñoz <mauro@cria.org.br>
+ * @date 2004-05-05
+ * $Id$
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2004 by CRIA -
+ * Centro de Referência em Informação Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef _BIOCLIMHH_
+#define _BIOCLIMHH_
+
+#include <om.hh>
+
+
+/****************************************************************/
+/*********************** Bioclim Distance ***********************/
+
+/** 
+ * Bioclimatic Envelope Algorithm with occurrence probability
+ * based on the distance to points' mean value.
+ *
+ */
+class BioclimDistance : public Algorithm
+{
+public:
+
+  BioclimDistance();
+  ~BioclimDistance();
+
+  // Inherited from Algorithm class.
+  int initialize();
+  int iterate();
+  int done();
+
+  Scalar getValue( Scalar *x );
+  int    getConvergence( Scalar *val );
+
+
+private:
+
+  /** Calculates the average point for all SampledData points.
+   *  There must be at least one point. */
+  Scalar *getMean( SampledData *points );
+
+  /** Calculates the standard deviation independently for each
+   *  dimension of 'points'.
+   *  There must be at least two points.
+   */
+  Scalar *getStandardDeviation( SampledData *points,
+                                Scalar *mean );
+
+
+  int _dim;   ///> Number of dimensions in environmental space
+
+  Scalar *_mean;    ///> Mean of sampled points.
+  Scalar *_std_dev; ///> Standard deviations for each variable.
+  Scalar _max_distance; ///> Standard deviation vector module.
+};
+
+
+#endif
+
