8ee5d18f995515f6ae7b1bf90bee1efc4ae2c876
scachett <scachett@02a2d6a7-111b-0410-992b-806f7cb7853c>
2004-09-22 21:44:11 +0000

    Added Python extension to Windows build.

---
+++ trunk/openmodeller/windows/vc6/BuildPython.bat
@@ -0,0 +1,12 @@
+echo on
+REM  Param1 is the base build directory ('%1')
+REM  Param2 is the input interface file ('%2')
+REM  Param3 is the output wrap file ('%3')
+
+echo Creating Python binding
+
+echo Deleting old wrap file: %3
+del %3 
+
+echo Creating SWIG interface for Python
+%SWIG_HOME%\swig.exe -c++ -python -o %3 %2
