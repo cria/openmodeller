55a849aebaa88c450ad1d03084b217430800d0f8
kruland <kruland@02a2d6a7-111b-0410-992b-806f7cb7853c>
2005-05-19 18:20:12 +0000

    Added an iterator class which iterates over a region in a map.  It uses internally integer counters in pixel space but quietly transforms the iterator into lon/lat using the geo-transformations associated with the underlying Raster and GeoTransform objects.

---
+++ src/openmodeller/env_io/MapIterator.cpp
@@ -0,0 +1,127 @@
+/**
+ * Definition of Map Iterator class
+ * 
+ * @file
+ * @author Kevin Ruland <kruland@ku.edu>
+ * @date 2005-05-19
+ * $Id$
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <env_io/map_iter.hh>
+#include <env_io/map.hh>
+#include <env_io/geo_transform.hh>
+
+using std::pair;
+
+// Default constructor for terminal
+MapIterator::MapIterator() :
+  isTerminal( true )
+{ }
+
+MapIterator::MapIterator( const Header& h,
+			  GeoTransform* gt ) :
+  isTerminal( false ),
+  gt( gt ),
+  h( h ),
+  xcur( 0 ),
+  ycur( 0 )
+{}
+
+pair<Coord,Coord> MapIterator::operator*() const
+{
+  pair<Coord,Coord> lonlat = h.convertXY2LonLat(xcur, ycur);
+  gt->transfOut( &lonlat.first, &lonlat.second);
+  return lonlat;
+}
+
+MapIterator& MapIterator::operator++()
+{
+  xcur++;
+  if (xcur >= h.xdim ) {
+    ycur++;
+    xcur = 0;
+  }
+  return *this;
+}
+
+MapIterator& MapIterator::operator--()
+{
+  xcur--;
+  if (xcur < 0 ) {
+    ycur--;
+    xcur = h.xdim-1;
+  }
+  return *this;
+}
+
+MapIterator MapIterator::operator++(int)
+{
+  MapIterator rval = *this;
+  operator++();
+  return rval;
+}
+
+MapIterator MapIterator::operator--(int)
+{
+  MapIterator rval = *this;
+  operator--();
+  return rval;
+}
+
+bool
+MapIterator::isPastBounds() const
+{
+
+  //
+  // Note, because the increment/decrement operators
+  // manipulate x first then y the foolowing should be true:
+  //   1.  xcur is in [xmin,xmax)
+  //   2.  ycur is the true test for out of bounds.
+  // Also, since I expect the majority of usage will be with operator++,
+  // I expect the condition ycur>= ymax, to be the most likely test.
+  return (ycur >= h.ydim) ||
+    (ycur < 0) ||
+    (xcur >= h.xdim) ||
+    (xcur < 0 );
+
+}
+
+bool
+operator==( const MapIterator& lhs, const MapIterator& rhs )
+{
+
+  if ( lhs.isTerminal ) {
+    return rhs.isPastBounds();
+  }
+  if ( rhs.isTerminal ) {
+    return lhs.isPastBounds();
+  }
+
+  return (lhs.xcur == rhs.xcur) && (lhs.ycur == rhs.ycur);
+
+}
+
+bool
+operator!=( const MapIterator& lhs, const MapIterator& rhs )
+{
+  return ! (lhs == rhs );
+}
---
+++ src/openmodeller/env_io/MapIterator.hh
@@ -0,0 +1,83 @@
+/**
+ * Declaration of Map Iterator class.
+ * 
+ * @file
+ * @author Kevin Ruland <kruland@ku.edu>
+ * @date 2005-05-19
+ * $Id$
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef MAP_ITER_HH
+#define MAP_ITER_HH
+
+#include <om_defs.hh>
+
+// For pair
+#include <utility>
+
+#include <env_io/header.hh>
+class GeoTransform;
+
+class MapIterator {
+
+  friend bool operator == ( const MapIterator&, const MapIterator& );
+  friend bool operator != ( const MapIterator&, const MapIterator& );
+
+public:
+  // Default constructor create a Terminal object which can be used in
+  // while (miter != MapIterator() {} loops.
+  MapIterator();
+
+  MapIterator( const Header& h, GeoTransform* gt );
+
+  // Compiler generated copy constructor, assignment operator and destructor
+  // are ok.
+  
+  // returns the current position in geo coordinates.
+  // first is x, second is y.
+  std::pair<Coord,Coord> operator*() const;
+
+  // prefix operator++
+  MapIterator& operator++();
+
+  // prefix operator--
+  MapIterator& operator--();
+
+  // postfix operator++
+  MapIterator operator++( int );
+
+  // postfix operator--
+  MapIterator operator--( int );
+
+private:
+  bool isPastBounds() const;
+  // If isTerminal is true, then this MapIterator object
+  // represents the terminal condition.
+  bool isTerminal;
+
+  GeoTransform *gt;
+  Header h;
+  int xcur;
+  int ycur;
+};
+
+#endif
