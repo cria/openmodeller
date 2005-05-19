354636b29f95770868b22a02d94f999e6e2ca77a
Renato <1180666+regiov@users.noreply.github.com>
2005-05-19 12:39:28 +0000

    Fixed python environment variable name, fixed file name of the main html output, and changed address where layers are located.

--- test/user/runtest.sh
+++ test/user/runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test ! -x data ; then 
-    wget http://www.cria.org.br/~ricardo/openModeller/acc_test_data.tar.gz
+    wget http://openmodeller.cria.org.br/download/acc_test_data.tar.gz
     tar -xzvf acc_test_data.tar.gz
 fi
 python run_all.py $@
