4af9e5d7ae32b90d16f720ec4f563d1c2738f25f
scachett <scachett@02a2d6a7-111b-0410-992b-806f7cb7853c>
2005-05-30 19:10:33 +0000

    Removed distinct lib and bin output directories from VC7 build. Now all debug output is dumped to build_debug and release output to build directories.

--- windows/vc7/CopyDll.bat
+++ windows/vc7/CopyDll.bat
@@ -3,9 +3,8 @@ REM  Param2 is the from directory ('%2')
 REM  Param3 is the output file base name without extension ('%3')
 
 if not exist ..\%1 mkdir ..\%1
-if not exist ..\%1\lib mkdir ..\%1\lib
 
-echo Installing library %2\%3.dll and additional files to .\%1\lib
-copy %2\%3.dll  ..\%1\lib
-copy %2\%3.exp  ..\%1\lib
-copy %2\%3.lib  ..\%1\lib
+echo Installing library %2\%3.dll and additional files to .\%1
+copy %2\%3.dll  ..\%1
+copy %2\%3.exp  ..\%1
+copy %2\%3.lib  ..\%1
