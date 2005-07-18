456de611a5ed9e27fcdd478b5e146a9c562463a7
Tim Sutton <tim@kartoza.com>
2005-07-18 15:57:25 +0000

    Initial attempt at producing man file for om_console using txt2man. Im having some problems convincing txt2man to lay the man page out nicely wo this should be considered a work in progress.

---
+++ doc/makeman.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Note you need the txt2man application installed to use this script!
+#
+# Tim Sutton 2005
+#
+
+txt2man -P om_console -t om_console om_console.txt > om_console.man
+
