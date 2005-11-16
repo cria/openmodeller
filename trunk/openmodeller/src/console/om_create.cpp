945988f550b87a8e57cd3ef32583daefdc3842a8
timlinux <timlinux@02a2d6a7-111b-0410-992b-806f7cb7853c>
2005-11-16 12:32:49 +0000

    Added om_create to cvs - thought I did this before but oh well....

---
+++ trunk/openmodeller/src/console/om_create.cpp
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *  A simple command line app to create an openModeller model
+ *    -------------------
+ *       begin                : November 2005
+ *       copyright            : (C) 2005 by T.Sutton, Kevin Ruland
+ *       email                : tim@linfiniti.com
+ ****************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "consolexml.cpp"
+using namespace std;
+
+int main( int argc, char **argv ) {
+    if (argc != 3) {
+      cout << "Usage: "
+          << argv[0]
+          << " <configfile-xml> a openmodeller xml request file "
+          << " <outputfilename> a file in which the xml outputs will be stored"
+          << endl;
+      return -1;
+    }
+  
+    std::string myRequest(argv[1]);
+    std::string myOutput=createModel(myRequest);
+  
+}
