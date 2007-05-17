c4d6c55f550d73f435c50237580e52969f95c70f
timlinux <timlinux@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-05-17 13:04:48 +0000

    Initial commit of ctest examples

---
+++ trunk/openmodeller/ctest/om_load_algs.cpp
@@ -0,0 +1,25 @@
+#include <openmodeller/om.hh>
+#include <iostream>
+int main( int argc, char **argv ) 
+{
+  AlgorithmFactory::searchDefaultDirs();
+  OpenModeller * mypOpenModeller;
+  const AlgMetadata **mypAlgorithmMetadataArray = mypOpenModeller->availableAlgorithms();
+  const AlgMetadata *mypAlgorithmMetadata = *mypAlgorithmMetadataArray;
+
+  unsigned int myCount=0;
+  while ( mypAlgorithmMetadata  )
+  {
+    std::cout << mypAlgorithmMetadata->name << std::endl;
+    *mypAlgorithmMetadataArray++;
+    mypAlgorithmMetadata = *mypAlgorithmMetadataArray;
+    ++myCount;
+  }
+  unsigned int myResult = 1; // assume fail
+  if (myCount > 0)
+  {
+    myResult = 0; //pass
+  }
+  return myResult;
+}
+
