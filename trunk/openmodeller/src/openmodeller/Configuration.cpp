e25bab3455a35920e394db5e725d86b2e77094e8
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-06-13 16:10:16 +0000

    Added missing header.

--- trunk/openmodeller/src/openmodeller/Configuration.cpp
+++ trunk/openmodeller/src/openmodeller/Configuration.cpp
@@ -1,3 +1,30 @@
+/**
+ * Definition of Configuration class
+ * 
+ * @author Kevin Ruland
+ * @date 2004-11-14
+ * $Id$
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
 #include <openmodeller/Configuration.hh>
 
 #include <iostream>
