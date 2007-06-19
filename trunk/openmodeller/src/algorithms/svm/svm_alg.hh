f5f1e09d3994fc288d52f019dcbc8af27145e3fc
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-06-19 13:05:51 +0000

    Included new algorithm: Support Vector Machines (C-SVC, Nu-SVC and one-class SVM).

---
+++ trunk/openmodeller/src/algorithms/svm/svm_alg.hh
@@ -0,0 +1,79 @@
+/**
+ * Declaration of SVM algorithm class.
+ * 
+ * @author Renato De Giovanni (renato [at] cria org br)
+ * $Id$
+ * 
+ * LICENSE INFORMATION 
+ * 
+ * Copyright(c) 2007 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef _SVM_ALGHH_
+#define _SVM_ALGHH_
+
+#include <openmodeller/om.hh>
+
+#include "svm.h"
+
+/**************************************************/
+/************* Support Vector Machines ************/
+
+/** 
+ * Algorithm to generate models using Support Vector Machines.
+ */
+class SvmAlgorithm : public AlgorithmImpl
+{
+public:
+
+  SvmAlgorithm();
+
+  virtual ~SvmAlgorithm();
+
+  // Inherited from Algorithm class.
+  int needNormalization( Scalar *min, Scalar *max ) const;
+
+  int initialize();
+  int iterate();
+  int done() const;
+
+  Scalar getValue( const Sample& x ) const;
+  int getConvergence( Scalar *val );
+
+protected:
+
+  svm_node * _getNode( const Sample& sample ) const;
+
+  virtual void _getConfiguration( ConfigurationPtr& ) const;
+
+  virtual void _setConfiguration( const ConstConfigurationPtr& );
+
+  bool _done;
+
+  int _num_layers;
+
+  svm_parameter _svm_parameter;
+  
+  svm_problem _svm_problem;
+
+  svm_model * _svm_model;
+};
+
+
+#endif
+
