1c51589bc800c85a19d22bf3bceb89c4b96ec1b8
timlinux <timlinux@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-02 18:21:49 +0000

    Template for creating new tests

---
+++ trunk/openmodeller/ctest/om_test_template.h
@@ -0,0 +1,59 @@
+/**
+ * Test class for <Insert Class Name Here> 
+ * 
+ * @author <Your Name>
+ * @date 2003-09-25
+ * $Id: OpenModeller.cpp 3201 2007-06-18 21:08:16Z rdg $
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/** \ingroup test
+* \brief A short description of this test goes here
+*/
+
+#ifndef <REPLACE_ME>_HH
+#define <REPLACE_ME>_HH
+
+#include "cxxtest/TestSuite.h"
+
+class <YourClassName> : public CxxTest :: TestSuite 
+{
+
+  public:
+    void setUp (){
+    }
+
+    void tearDown (){
+    }
+
+    void test1 (){
+      std::cout << "" << std::endl;
+
+      TS_ASSERT(<yourcriteria> == <yourothercriteria>);
+    }
+
+  private:
+    <test vars here>
+};
+
+
+#endif
+
