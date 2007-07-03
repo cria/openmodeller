68564ebdb15d76c416886be43aabe3c93a3bd1cc
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-03 19:04:18 +0000

    Added tests for icstring.

--- ctest/om_test_sample.cpp
+++ ctest/om_test_sample.cpp
@@ -16,147 +16,147 @@ int main() {
 }
 #include "om_test_sample.h"
 
-static FixtureSuite suite_FixtureSuite;
+static test_Sample suite_test_Sample;
 
-static CxxTest::List Tests_FixtureSuite = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_FixtureSuite( "om_test_sample.h", 11, "FixtureSuite", suite_FixtureSuite, Tests_FixtureSuite );
+static CxxTest::List Tests_test_Sample = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_test_Sample( "om_test_sample.h", 42, "test_Sample", suite_test_Sample, Tests_test_Sample );
 
-static class TestDescription_FixtureSuite_test1 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test1() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 29, "test1" ) {}
- void runTest() { suite_FixtureSuite.test1(); }
-} testDescription_FixtureSuite_test1;
+ TestDescription_test_Sample_test1() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 60, "test1" ) {}
+ void runTest() { suite_test_Sample.test1(); }
+} testDescription_test_Sample_test1;
 
-static class TestDescription_FixtureSuite_test2 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test2 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test2() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 36, "test2" ) {}
- void runTest() { suite_FixtureSuite.test2(); }
-} testDescription_FixtureSuite_test2;
+ TestDescription_test_Sample_test2() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 67, "test2" ) {}
+ void runTest() { suite_test_Sample.test2(); }
+} testDescription_test_Sample_test2;
 
-static class TestDescription_FixtureSuite_test3 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test3 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test3() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 43, "test3" ) {}
- void runTest() { suite_FixtureSuite.test3(); }
-} testDescription_FixtureSuite_test3;
+ TestDescription_test_Sample_test3() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 74, "test3" ) {}
+ void runTest() { suite_test_Sample.test3(); }
+} testDescription_test_Sample_test3;
 
-static class TestDescription_FixtureSuite_test4 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test4 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test4() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 51, "test4" ) {}
- void runTest() { suite_FixtureSuite.test4(); }
-} testDescription_FixtureSuite_test4;
+ TestDescription_test_Sample_test4() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 82, "test4" ) {}
+ void runTest() { suite_test_Sample.test4(); }
+} testDescription_test_Sample_test4;
 
-static class TestDescription_FixtureSuite_test5 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test5 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test5() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 63, "test5" ) {}
- void runTest() { suite_FixtureSuite.test5(); }
-} testDescription_FixtureSuite_test5;
+ TestDescription_test_Sample_test5() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 94, "test5" ) {}
+ void runTest() { suite_test_Sample.test5(); }
+} testDescription_test_Sample_test5;
 
-static class TestDescription_FixtureSuite_test6 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test6 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test6() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 93, "test6" ) {}
- void runTest() { suite_FixtureSuite.test6(); }
-} testDescription_FixtureSuite_test6;
+ TestDescription_test_Sample_test6() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 124, "test6" ) {}
+ void runTest() { suite_test_Sample.test6(); }
+} testDescription_test_Sample_test6;
 
-static class TestDescription_FixtureSuite_test7 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test7 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test7() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 106, "test7" ) {}
- void runTest() { suite_FixtureSuite.test7(); }
-} testDescription_FixtureSuite_test7;
+ TestDescription_test_Sample_test7() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 137, "test7" ) {}
+ void runTest() { suite_test_Sample.test7(); }
+} testDescription_test_Sample_test7;
 
-static class TestDescription_FixtureSuite_test8 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test8 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test8() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 119, "test8" ) {}
- void runTest() { suite_FixtureSuite.test8(); }
-} testDescription_FixtureSuite_test8;
+ TestDescription_test_Sample_test8() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 150, "test8" ) {}
+ void runTest() { suite_test_Sample.test8(); }
+} testDescription_test_Sample_test8;
 
-static class TestDescription_FixtureSuite_test9 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test9 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test9() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 134, "test9" ) {}
- void runTest() { suite_FixtureSuite.test9(); }
-} testDescription_FixtureSuite_test9;
+ TestDescription_test_Sample_test9() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 165, "test9" ) {}
+ void runTest() { suite_test_Sample.test9(); }
+} testDescription_test_Sample_test9;
 
-static class TestDescription_FixtureSuite_test10 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test10 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test10() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 153, "test10" ) {}
- void runTest() { suite_FixtureSuite.test10(); }
-} testDescription_FixtureSuite_test10;
+ TestDescription_test_Sample_test10() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 184, "test10" ) {}
+ void runTest() { suite_test_Sample.test10(); }
+} testDescription_test_Sample_test10;
 
-static class TestDescription_FixtureSuite_test11 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test11 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test11() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 171, "test11" ) {}
- void runTest() { suite_FixtureSuite.test11(); }
-} testDescription_FixtureSuite_test11;
+ TestDescription_test_Sample_test11() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 202, "test11" ) {}
+ void runTest() { suite_test_Sample.test11(); }
+} testDescription_test_Sample_test11;
 
-static class TestDescription_FixtureSuite_test12 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test12 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test12() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 187, "test12" ) {}
- void runTest() { suite_FixtureSuite.test12(); }
-} testDescription_FixtureSuite_test12;
+ TestDescription_test_Sample_test12() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 218, "test12" ) {}
+ void runTest() { suite_test_Sample.test12(); }
+} testDescription_test_Sample_test12;
 
-static class TestDescription_FixtureSuite_test13 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test13 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test13() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 203, "test13" ) {}
- void runTest() { suite_FixtureSuite.test13(); }
-} testDescription_FixtureSuite_test13;
+ TestDescription_test_Sample_test13() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 234, "test13" ) {}
+ void runTest() { suite_test_Sample.test13(); }
+} testDescription_test_Sample_test13;
 
-static class TestDescription_FixtureSuite_test14 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test14 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test14() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 219, "test14" ) {}
- void runTest() { suite_FixtureSuite.test14(); }
-} testDescription_FixtureSuite_test14;
+ TestDescription_test_Sample_test14() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 250, "test14" ) {}
+ void runTest() { suite_test_Sample.test14(); }
+} testDescription_test_Sample_test14;
 
-static class TestDescription_FixtureSuite_test15 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test15 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test15() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 235, "test15" ) {}
- void runTest() { suite_FixtureSuite.test15(); }
-} testDescription_FixtureSuite_test15;
+ TestDescription_test_Sample_test15() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 266, "test15" ) {}
+ void runTest() { suite_test_Sample.test15(); }
+} testDescription_test_Sample_test15;
 
-static class TestDescription_FixtureSuite_test16 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test16 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test16() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 251, "test16" ) {}
- void runTest() { suite_FixtureSuite.test16(); }
-} testDescription_FixtureSuite_test16;
+ TestDescription_test_Sample_test16() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 282, "test16" ) {}
+ void runTest() { suite_test_Sample.test16(); }
+} testDescription_test_Sample_test16;
 
-static class TestDescription_FixtureSuite_test17 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test17 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test17() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 271, "test17" ) {}
- void runTest() { suite_FixtureSuite.test17(); }
-} testDescription_FixtureSuite_test17;
+ TestDescription_test_Sample_test17() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 302, "test17" ) {}
+ void runTest() { suite_test_Sample.test17(); }
+} testDescription_test_Sample_test17;
 
-static class TestDescription_FixtureSuite_test18 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test18 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test18() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 287, "test18" ) {}
- void runTest() { suite_FixtureSuite.test18(); }
-} testDescription_FixtureSuite_test18;
+ TestDescription_test_Sample_test18() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 318, "test18" ) {}
+ void runTest() { suite_test_Sample.test18(); }
+} testDescription_test_Sample_test18;
 
-static class TestDescription_FixtureSuite_test19 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test19 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test19() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 309, "test19" ) {}
- void runTest() { suite_FixtureSuite.test19(); }
-} testDescription_FixtureSuite_test19;
+ TestDescription_test_Sample_test19() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 340, "test19" ) {}
+ void runTest() { suite_test_Sample.test19(); }
+} testDescription_test_Sample_test19;
 
-static class TestDescription_FixtureSuite_test20 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test20 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test20() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 328, "test20" ) {}
- void runTest() { suite_FixtureSuite.test20(); }
-} testDescription_FixtureSuite_test20;
+ TestDescription_test_Sample_test20() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 359, "test20" ) {}
+ void runTest() { suite_test_Sample.test20(); }
+} testDescription_test_Sample_test20;
 
-static class TestDescription_FixtureSuite_test21 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test21 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test21() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 345, "test21" ) {}
- void runTest() { suite_FixtureSuite.test21(); }
-} testDescription_FixtureSuite_test21;
+ TestDescription_test_Sample_test21() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 376, "test21" ) {}
+ void runTest() { suite_test_Sample.test21(); }
+} testDescription_test_Sample_test21;
 
-static class TestDescription_FixtureSuite_test22 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test22 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test22() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 362, "test22" ) {}
- void runTest() { suite_FixtureSuite.test22(); }
-} testDescription_FixtureSuite_test22;
+ TestDescription_test_Sample_test22() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 393, "test22" ) {}
+ void runTest() { suite_test_Sample.test22(); }
+} testDescription_test_Sample_test22;
 
-static class TestDescription_FixtureSuite_test23 : public CxxTest::RealTestDescription {
+static class TestDescription_test_Sample_test23 : public CxxTest::RealTestDescription {
 public:
- TestDescription_FixtureSuite_test23() : CxxTest::RealTestDescription( Tests_FixtureSuite, suiteDescription_FixtureSuite, 375, "test23" ) {}
- void runTest() { suite_FixtureSuite.test23(); }
-} testDescription_FixtureSuite_test23;
+ TestDescription_test_Sample_test23() : CxxTest::RealTestDescription( Tests_test_Sample, suiteDescription_test_Sample, 406, "test23" ) {}
+ void runTest() { suite_test_Sample.test23(); }
+} testDescription_test_Sample_test23;
 
 #include <cxxtest/Root.cpp>
--- ctest/om_test_sample.h
+++ ctest/om_test_sample.h
@@ -1,3 +1,34 @@
+/**
+ * Test class for Sample
+ * 
+ * @authors Ricardo Scachetti Pereira and Albert Massayuki Kuniyoshi
+ * @date 2007-07-03
+ * $Id: OpenModeller.cpp 3201 2007-06-18 21:08:16Z rdg $
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/** \ingroup test
+* \brief A short description of this test goes here
+*/
+
 #ifndef TEST_SAMPLE_HH
 #define TEST_SAMPLE_HH
 
@@ -8,7 +39,7 @@
 #include <sstream>
 #include <iostream>
 
-class FixtureSuite : public CxxTest :: TestSuite 
+class test_Sample : public CxxTest :: TestSuite 
 {
 
 	public:
@@ -395,6 +426,7 @@ class FixtureSuite : public CxxTest :: TestSuite
 				Sample *b;
 				Sample *c;
 				Sample *d;
+
 };
 
 
