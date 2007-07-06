94d20c656d42dd205bdfab6c10540b3cba674e2d
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-06 01:50:59 +0000

    Tests for Random have been completed

--- trunk/openmodeller/ctest/om_test_random.cpp
+++ trunk/openmodeller/ctest/om_test_random.cpp
@@ -23,14 +23,86 @@ CxxTest::StaticSuiteDescription suiteDescription_test_Random( "om_test_random.h"
 
 static class TestDescription_test_Random_test1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_Random_test1() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 55, "test1" ) {}
+ TestDescription_test_Random_test1() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 59, "test1" ) {}
  void runTest() { suite_test_Random.test1(); }
 } testDescription_test_Random_test1;
 
 static class TestDescription_test_Random_test2 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_Random_test2() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 63, "test2" ) {}
+ TestDescription_test_Random_test2() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 67, "test2" ) {}
  void runTest() { suite_test_Random.test2(); }
 } testDescription_test_Random_test2;
+
+static class TestDescription_test_Random_test3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test3() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 75, "test3" ) {}
+ void runTest() { suite_test_Random.test3(); }
+} testDescription_test_Random_test3;
+
+static class TestDescription_test_Random_test4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test4() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 82, "test4" ) {}
+ void runTest() { suite_test_Random.test4(); }
+} testDescription_test_Random_test4;
+
+static class TestDescription_test_Random_test5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test5() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 90, "test5" ) {}
+ void runTest() { suite_test_Random.test5(); }
+} testDescription_test_Random_test5;
+
+static class TestDescription_test_Random_test7 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test7() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 98, "test7" ) {}
+ void runTest() { suite_test_Random.test7(); }
+} testDescription_test_Random_test7;
+
+static class TestDescription_test_Random_test8 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test8() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 106, "test8" ) {}
+ void runTest() { suite_test_Random.test8(); }
+} testDescription_test_Random_test8;
+
+static class TestDescription_test_Random_test9 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test9() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 114, "test9" ) {}
+ void runTest() { suite_test_Random.test9(); }
+} testDescription_test_Random_test9;
+
+static class TestDescription_test_Random_test10 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test10() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 122, "test10" ) {}
+ void runTest() { suite_test_Random.test10(); }
+} testDescription_test_Random_test10;
+
+static class TestDescription_test_Random_test11 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test11() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 130, "test11" ) {}
+ void runTest() { suite_test_Random.test11(); }
+} testDescription_test_Random_test11;
+
+static class TestDescription_test_Random_test12 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test12() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 138, "test12" ) {}
+ void runTest() { suite_test_Random.test12(); }
+} testDescription_test_Random_test12;
+
+static class TestDescription_test_Random_test13 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test13() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 146, "test13" ) {}
+ void runTest() { suite_test_Random.test13(); }
+} testDescription_test_Random_test13;
+
+static class TestDescription_test_Random_test14 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test14() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 154, "test14" ) {}
+ void runTest() { suite_test_Random.test14(); }
+} testDescription_test_Random_test14;
+
+static class TestDescription_test_Random_test15 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Random_test15() : CxxTest::RealTestDescription( Tests_test_Random, suiteDescription_test_Random, 162, "test15" ) {}
+ void runTest() { suite_test_Random.test15(); }
+} testDescription_test_Random_test15;
 
 #include <cxxtest/Root.cpp>
