e38f194154424d432b7f631eae7279c3438d0f93
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-10 13:12:13 +0000

    Added om_test_exceptions.cpp and om_test_exceptions.h

---
+++ trunk/openmodeller/ctest/om_test_exceptions.h
@@ -0,0 +1,67 @@
+/**
+ * Test class for Excepetions
+ * 
+ * @author Albert Massayuki Kuniyoshi
+ * @date 2007-07-06
+ * $Id: OpenModeller.cpp 3201 2007-06-18 21:08:16Z rdg $
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/** \ingroup test
+* \brief A short description of this test goes here
+*/
+
+#ifndef TEST_EXCEPTIONS_HH
+#define TEST_EXCEPTIONS_HH
+
+#include "cxxtest/TestSuite.h"
+#include "Exceptions.hh"
+#include <string>
+#include <stdexcept>
+
+class test_Random : public CxxTest :: TestSuite 
+{
+
+	public:
+		void setUp (){
+				b = new std::string;
+				*b = "Testando...";
+				a = new OmException(*b);
+				}
+
+		void tearDown (){
+				delete a;
+				delete b;
+				}
+
+		void test1 (){
+				std::cout << std::endl;
+				std::cout << "Testing OmException( const std::string& msg ) ..." << std::endl;
+				TS_ASSERT_EQUALS(1,1);	
+				}
+
+	private:
+			OmException *a;
+			std::string *b;
+
+};
+
+#endif
