3d12907dec841ea34fec04eed3fde335ffe1956c
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-16 18:05:37 +0000

    Added some tests for Configuration

--- trunk/openmodeller/ctest/om_test_configuration.cpp
+++ trunk/openmodeller/ctest/om_test_configuration.cpp
@@ -19,12 +19,24 @@ int main() {
 static test_Configuration suite_test_Configuration;
 
 static CxxTest::List Tests_test_Configuration = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_test_Configuration( "om_test_configuration.h", 39, "test_Configuration", suite_test_Configuration, Tests_test_Configuration );
+CxxTest::StaticSuiteDescription suiteDescription_test_Configuration( "om_test_configuration.h", 42, "test_Configuration", suite_test_Configuration, Tests_test_Configuration );
 
 static class TestDescription_test_Configuration_test1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_Configuration_test1() : CxxTest::RealTestDescription( Tests_test_Configuration, suiteDescription_test_Configuration, 50, "test1" ) {}
+ TestDescription_test_Configuration_test1() : CxxTest::RealTestDescription( Tests_test_Configuration, suiteDescription_test_Configuration, 56, "test1" ) {}
  void runTest() { suite_test_Configuration.test1(); }
 } testDescription_test_Configuration_test1;
+
+static class TestDescription_test_Configuration_test2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Configuration_test2() : CxxTest::RealTestDescription( Tests_test_Configuration, suiteDescription_test_Configuration, 67, "test2" ) {}
+ void runTest() { suite_test_Configuration.test2(); }
+} testDescription_test_Configuration_test2;
+
+static class TestDescription_test_Configuration_test3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Configuration_test3() : CxxTest::RealTestDescription( Tests_test_Configuration, suiteDescription_test_Configuration, 75, "test3" ) {}
+ void runTest() { suite_test_Configuration.test3(); }
+} testDescription_test_Configuration_test3;
 
 #include <cxxtest/Root.cpp>
--- trunk/openmodeller/ctest/om_test_configuration.h
+++ trunk/openmodeller/ctest/om_test_configuration.h
@@ -33,7 +33,10 @@
 #define TEST_CONFIGURATION_HH
 
 #include "cxxtest/TestSuite.h"
-#include "Configuration.hh"
+#include <openmodeller/Configuration.hh>
+#include <openmodeller/Exceptions.hh>
+#include <openmodeller/Sample.hh>
+#include <sstream>
 #include <string>
 
 class test_Configuration : public CxxTest :: TestSuite 
@@ -41,6 +44,9 @@ class test_Configuration : public CxxTest :: TestSuite
 	public:
 		void setUp (){
 				a = new ConfigurationImpl();
+				subs = new Configuration::subsection_list;
+				attrs = new Configuration::attribute_list;
+				word = new std::string;
 				}
 
 		void tearDown (){
@@ -52,10 +58,33 @@ class test_Configuration : public CxxTest :: TestSuite
 				std::cout << "Testing Default Constructor..." << std::endl;
 				TS_ASSERT(a->getName().empty());
 				TS_ASSERT(a->getValue().empty());
+				*subs = a->getAllSubsections();
+				TS_ASSERT(subs->empty());
+				*attrs = a->getAllAttributes();
+				TS_ASSERT(attrs->empty());
 				}
-	private:
-			ConfigurationImpl *a;
 
+		void test2 (){
+				std::cout << std::endl;
+				std::cout << "Testing SetName..." << std::endl;
+				*word = "openModeller";
+				a->setName(*word);
+				TS_ASSERT(a->getName() == *word);
+				}
+
+		void test3 (){
+				std::cout << std::endl;
+				std::cout << "Testing SetValue with no whitespace..." << std::endl;
+				*word = "openModeller";
+				a->setValue(*word);
+				TS_ASSERT(a->getValue() == *word);
+				}
+
+		private:
+			ConfigurationImpl *a;
+			Configuration::subsection_list *subs;
+			Configuration::attribute_list *attrs;
+			std::string *word;
 };
 
 #endif
