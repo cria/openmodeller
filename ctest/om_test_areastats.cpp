fb5fc49aa3d97476b26c18edc1b754b0a4bb9168
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-30 18:48:50 +0000

    Tests for AreaStats have been completed

--- ctest/om_test_areastats.cpp
+++ ctest/om_test_areastats.cpp
@@ -19,48 +19,54 @@ int main() {
 static test_AreaStats suite_test_AreaStats;
 
 static CxxTest::List Tests_test_AreaStats = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_test_AreaStats( "om_test_areastats.h", 39, "test_AreaStats", suite_test_AreaStats, Tests_test_AreaStats );
+CxxTest::StaticSuiteDescription suiteDescription_test_AreaStats( "om_test_areastats.h", 41, "test_AreaStats", suite_test_AreaStats, Tests_test_AreaStats );
 
 static class TestDescription_test_AreaStats_test1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test1() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 52, "test1" ) {}
+ TestDescription_test_AreaStats_test1() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 56, "test1" ) {}
  void runTest() { suite_test_AreaStats.test1(); }
 } testDescription_test_AreaStats_test1;
 
 static class TestDescription_test_AreaStats_test2 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test2() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 62, "test2" ) {}
+ TestDescription_test_AreaStats_test2() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 66, "test2" ) {}
  void runTest() { suite_test_AreaStats.test2(); }
 } testDescription_test_AreaStats_test2;
 
 static class TestDescription_test_AreaStats_test3 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test3() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 72, "test3" ) {}
+ TestDescription_test_AreaStats_test3() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 76, "test3" ) {}
  void runTest() { suite_test_AreaStats.test3(); }
 } testDescription_test_AreaStats_test3;
 
 static class TestDescription_test_AreaStats_test4 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test4() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 83, "test4" ) {}
+ TestDescription_test_AreaStats_test4() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 87, "test4" ) {}
  void runTest() { suite_test_AreaStats.test4(); }
 } testDescription_test_AreaStats_test4;
 
 static class TestDescription_test_AreaStats_test5 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test5() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 94, "test5" ) {}
+ TestDescription_test_AreaStats_test5() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 98, "test5" ) {}
  void runTest() { suite_test_AreaStats.test5(); }
 } testDescription_test_AreaStats_test5;
 
 static class TestDescription_test_AreaStats_test6 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test6() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 105, "test6" ) {}
+ TestDescription_test_AreaStats_test6() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 109, "test6" ) {}
  void runTest() { suite_test_AreaStats.test6(); }
 } testDescription_test_AreaStats_test6;
 
 static class TestDescription_test_AreaStats_test7 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AreaStats_test7() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 116, "test7" ) {}
+ TestDescription_test_AreaStats_test7() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 120, "test7" ) {}
  void runTest() { suite_test_AreaStats.test7(); }
 } testDescription_test_AreaStats_test7;
+
+static class TestDescription_test_AreaStats_test8 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AreaStats_test8() : CxxTest::RealTestDescription( Tests_test_AreaStats, suiteDescription_test_AreaStats, 131, "test8" ) {}
+ void runTest() { suite_test_AreaStats.test8(); }
+} testDescription_test_AreaStats_test8;
 
 #include <cxxtest/Root.cpp>
