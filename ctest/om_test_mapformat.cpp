62c341a571537b63f6cf35d633eb87ab00625289
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-31 17:09:34 +0000

    Added test files for MapFormat

---
+++ ctest/om_test_mapformat.cpp
@@ -0,0 +1,30 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#define _CXXTEST_HAVE_STD
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/ErrorPrinter.h>
+
+int main() {
+ return CxxTest::ErrorPrinter().run();
+}
+#include "om_test_mapformat.h"
+
+static test_MapFormat suite_test_MapFormat;
+
+static CxxTest::List Tests_test_MapFormat = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_test_MapFormat( "om_test_mapformat.h", 38, "test_MapFormat", suite_test_MapFormat, Tests_test_MapFormat );
+
+static class TestDescription_test_MapFormat_test1 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_MapFormat_test1() : CxxTest::RealTestDescription( Tests_test_MapFormat, suiteDescription_test_MapFormat, 49, "test1" ) {}
+ void runTest() { suite_test_MapFormat.test1(); }
+} testDescription_test_MapFormat_test1;
+
+#include <cxxtest/Root.cpp>
