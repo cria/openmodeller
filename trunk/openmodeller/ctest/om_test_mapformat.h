9e8933f8944583f0fd2dc2d523e444e21c613e3d
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-07-31 17:09:34 +0000

    Added test files for MapFormat

---
+++ trunk/openmodeller/ctest/om_test_mapformat.h
@@ -0,0 +1,58 @@
+/**
+ * Test class for MapFormat
+ * 
+ * @author Albert Massayuki Kuniyoshi
+ * @date 2007-07-30
+ * $Id: OpenModeller.cpp 3201 2007-06-18 21:08:16Z rdg $
+ *
+ * LICENSE INFORMATION
+ * 
+ * Copyright(c) 2003 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/** \ingroup test_MapFormat
+* \brief Test for MapFormat
+*/
+
+#ifndef TEST_MAPFORMAT_HH
+#define TEST_MAPFORMAT_HH
+
+#include "cxxtest/TestSuite.h"
+#include "MapFormat.hh"
+
+class test_MapFormat: public CxxTest :: TestSuite 
+{
+	public:
+		void setUp (){
+
+				}
+
+		void tearDown (){
+
+				}
+
+		void test1 (){
+				std::cout << std::endl;
+				std::cout << "Testing ..." << std::endl;
+
+				}
+
+		private:
+
+};
+#endif
