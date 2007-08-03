7a3a5372e38b9314b30f7c2e0cd4d17dce5cc70e
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-03 17:38:42 +0000

    Tests for Exceptions have been completed

--- ctest/om_test_exceptions.cpp
+++ ctest/om_test_exceptions.cpp
@@ -27,4 +27,64 @@ public:
  void runTest() { suite_test_Exceptions.test1(); }
 } testDescription_test_Exceptions_test1;
 
+static class TestDescription_test_Exceptions_test2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test2() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 61, "test2" ) {}
+ void runTest() { suite_test_Exceptions.test2(); }
+} testDescription_test_Exceptions_test2;
+
+static class TestDescription_test_Exceptions_test3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test3() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 68, "test3" ) {}
+ void runTest() { suite_test_Exceptions.test3(); }
+} testDescription_test_Exceptions_test3;
+
+static class TestDescription_test_Exceptions_test4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test4() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 77, "test4" ) {}
+ void runTest() { suite_test_Exceptions.test4(); }
+} testDescription_test_Exceptions_test4;
+
+static class TestDescription_test_Exceptions_test5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test5() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 86, "test5" ) {}
+ void runTest() { suite_test_Exceptions.test5(); }
+} testDescription_test_Exceptions_test5;
+
+static class TestDescription_test_Exceptions_test6 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test6() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 93, "test6" ) {}
+ void runTest() { suite_test_Exceptions.test6(); }
+} testDescription_test_Exceptions_test6;
+
+static class TestDescription_test_Exceptions_test7 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test7() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 100, "test7" ) {}
+ void runTest() { suite_test_Exceptions.test7(); }
+} testDescription_test_Exceptions_test7;
+
+static class TestDescription_test_Exceptions_test8 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test8() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 109, "test8" ) {}
+ void runTest() { suite_test_Exceptions.test8(); }
+} testDescription_test_Exceptions_test8;
+
+static class TestDescription_test_Exceptions_test9 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test9() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 116, "test9" ) {}
+ void runTest() { suite_test_Exceptions.test9(); }
+} testDescription_test_Exceptions_test9;
+
+static class TestDescription_test_Exceptions_test10 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test10() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 123, "test10" ) {}
+ void runTest() { suite_test_Exceptions.test10(); }
+} testDescription_test_Exceptions_test10;
+
+static class TestDescription_test_Exceptions_test11 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_Exceptions_test11() : CxxTest::RealTestDescription( Tests_test_Exceptions, suiteDescription_test_Exceptions, 130, "test11" ) {}
+ void runTest() { suite_test_Exceptions.test11(); }
+} testDescription_test_Exceptions_test11;
+
 #include <cxxtest/Root.cpp>
