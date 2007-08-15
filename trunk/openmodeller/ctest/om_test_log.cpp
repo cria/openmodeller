be5e0889e633a53a23b7649a38ea11b8fa630deb
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-15 18:23:37 +0000

    Added one more test for configuration

--- trunk/openmodeller/ctest/om_test_log.cpp
+++ trunk/openmodeller/ctest/om_test_log.cpp
@@ -1,26 +1,26 @@
 #include <openmodeller/om.hh>
 #include <iostream>
-class MyLog : public Log::LogCallback 
+class MyLog : public Log::LogCallback
 {
-  void operator()( Log::Level l, const std::string& msg ) 
-  {
-    std::cout << msg;
-  }
+void operator()( Log::Level l, const std::string& msg )
+{
+std::cout << msg;
+}
 };
 
-int main( int argc, char **argv ) 
+int main( int argc, char **argv )
 {
-  try {
-    Log::instance()->setLevel( Log::Debug );
-    Log::instance()->setCallback( new MyLog() );
-    Log::instance()->debug( "sample debug message\n" );
-    Log::instance()->info( "sample info message\n" );
-    Log::instance()->warn( "sample warn message.\n");
-    //Log::instance()->error(0, "sample error message.\n");
-  }
-  catch (...)
-  {
-    return 1; //fail
-  }
-  return 0; //pass
+try {
+Log::instance()->setLevel( Log::Debug );
+Log::instance()->setCallback( new MyLog() );
+Log::instance()->debug( "sample debug message\n" );
+Log::instance()->info( "sample info message\n" );
+Log::instance()->warn( "sample warn message.\n");
+//Log::instance()->error(0, "sample error message.\n");
+}
+catch (...)
+{
+return 1; //fail
+}
+return 0; //pass
 }
