443027c87d6db271d997d3323e8b1e6a61453e14
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-21 14:35:48 +0000

    Disabled debug message.

--- trunk/openmodeller/src/openmodeller/env_io/RasterGdal.cpp
+++ trunk/openmodeller/src/openmodeller/env_io/RasterGdal.cpp
@@ -278,7 +278,7 @@ RasterGdal::open( char mode )
   f_hdr.ymax = (Scalar) cf[3];
   f_hdr.ymin = (Scalar) (cf[3] + xd * cf[4] + yd * cf[5]);
 
-  Log::instance()->debug( "Raster boundaries: xmin=%f, xmax=%f, ymin=%f, ymax=%f \n", f_hdr.xmin, f_hdr.xmax, f_hdr.ymin, f_hdr.ymax );
+  //Log::instance()->debug( "Raster boundaries: xmin=%f, xmax=%f, ymin=%f, ymax=%f \n", f_hdr.xmin, f_hdr.xmax, f_hdr.ymin, f_hdr.ymax );
 
   f_hdr.calculateCell();
 
