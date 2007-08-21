e57cebc4b6078c29f1880b4cf98369c6d13ec904
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-21 19:18:20 +0000

    Added more tests for Occurrence (Occurrence constructors)

--- ctest/om_test_occurrence.h
+++ ctest/om_test_occurrence.h
@@ -26,33 +26,82 @@
  */
 
 /** \ingroup test
-* \brief Test for Occurrence Class
+* \brief Test for OccurrenceImpl Class
 */
 
 #ifndef TEST_OCCURRENCE_HH
 #define TEST_OCCURRENCE_HH
 #include "cxxtest/TestSuite.h"
 #include <openmodeller/Occurrence.hh>
+#include <openmodeller/Sample.hh>
+#include <openmodeller/om_defs.hh>
+#include <string.h>
 
 class test_Occurrence : public CxxTest :: TestSuite 
 {
 
   public:
     void setUp (){
-
+      name = new std::string;
+      *name = "id";
+      A = new OccurrenceImpl();
+      B = new OccurrenceImpl(*name,Coord(1.0),Coord(2.0),Scalar(3.0),Scalar(4.0));
     }
 
     void tearDown (){
-
+      delete name;
+      delete A;
+      delete B;
     }
 
     void test1 (){
-      std::cout << "Testing ..." << std::endl;
+      std::cout << "Testing Occurrence default constructor ..." << std::endl;
+      TS_ASSERT((A->id()).empty());
+      TS_ASSERT(A->x()==Coord(0.0));
+      TS_ASSERT(A->y()==Coord(0.0));
+      TS_ASSERT(A->error()==Scalar(0.0));
+      TS_ASSERT(A->abundance()==Scalar(0.0));
+      TS_ASSERT(A->attributes()==Sample());
+      TS_ASSERT(A->environment()==Sample());
+      TS_ASSERT(A->originalEnvironment()==Sample());
+    }
+
+    void test2 (){
+      std::cout << "Testing Occurrence constructor with uncertanty I ..." << std::endl;
+      TS_ASSERT(B->id()==*name);
+      TS_ASSERT(B->x()==Coord(1.0));
+      TS_ASSERT(B->y()==Coord(2.0));
+      TS_ASSERT(B->error()==Scalar(3.0));
+      TS_ASSERT(B->abundance()==Scalar(4.0));
+      TS_ASSERT(B->attributes()==Sample());
+      TS_ASSERT(B->environment()==Sample());
+      TS_ASSERT(B              ->originalEnvironment()==Sample());
+    }
+
+    void test3 (){
+      std::cout << "Testing Occurrence constructor with uncertanty using std::vector ..." << std::endl;
+      TS_ASSERT(true);
+    }
+
+    void test4 (){
+      std::cout << "Testing Occurrence constructor with uncertanty II ..." << std::endl;
+      TS_ASSERT(true);
+    }
+
+    void test5 (){
+      std::cout << "Testing Occurrence constructor without uncertanty..." << std::endl;
+      TS_ASSERT(true);
+    }
+
+    void test6 (){
+      std::cout << "Testing Occurrence constructor by OccurrenceImpl reference ..." << std::endl;
       TS_ASSERT(true);
     }
 
   private:
-
+      std::string *name;
+      OccurrenceImpl *A;
+      OccurrenceImpl *B;
 };
 
 #endif
