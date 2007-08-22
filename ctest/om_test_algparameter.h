503c4afb0d54c3d7d38eb9e8e57fd4874670db21
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-22 18:56:42 +0000

    Added more tests for AlgParameter

--- ctest/om_test_algparameter.h
+++ ctest/om_test_algparameter.h
@@ -39,21 +39,68 @@ class test_AlgParameter : public CxxTest :: TestSuite
 
   public:
     void setUp (){
-
+                 A = new AlgParameter();
+                 B = new AlgParameter("id","value");
+                 C = new AlgParameter(*A);
     }
 
     void tearDown (){
-
+                 delete A;
+                 delete B;
+                 delete C;
     }
 
     void test1 (){
-      std::cout << "Testing ..." << std::endl;
+      std::cout << "Testing AlgParameter default constructor ..." << std::endl;
+                TS_ASSERT(A->id()==0);
+                TS_ASSERT(A->value()==0);
+    }
+
+    void test2 (){
+      std::cout << "Testing AlgParameter constructor ..." << std::endl;
+                TS_WARN("Need to fix characters!");
+/*
+                TS_ASSERT_EQUALS(B->id(),"id");
+                TS_ASSERT_EQUALS(B->value(),"value");
+*/
+    }
+
+    void test3 (){
+      std::cout << "Testing AlgParameter constructor ..." << std::endl;
+                TS_ASSERT(C->id()==0);
+                TS_ASSERT(C->value()==0);
+    }
+
+    void test4 (){
+      std::cout << "Testing Algparameter operator=(const AlgParameter &) ..." << std::endl;
+                B->operator=(*A);
+                TS_ASSERT_EQUALS(B->id(),A->id());
+                TS_ASSERT_EQUALS(B->value(),A->value());
+    }
+
+    void test5 (){
+      std::cout << "Testing setId(char const *id) ..." << std::endl;
+                A->setId("id_");
+                TS_ASSERT(A->id()=="id_");
+                TS_ASSERT(A->value()=="value");
+    }
+
+    void test6 (){
+      std::cout << "Testing setValue(char const *val) ..." << std::endl;
+                A->setValue("value_");
+                TS_ASSERT(A->id()=="id_");
+                TS_ASSERT(A->value()=="value_");
+    }
+
+    void test7 (){
+      std::cout << "Testing setValue(double val) ..." << std::endl;
 
     }
 
-
   private:
-
+             AlgParameter *A;
+             AlgParameter *B;
+             AlgParameter *C;
 };
 
 #endif
