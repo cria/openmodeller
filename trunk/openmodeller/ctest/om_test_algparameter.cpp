0d7259c4dda813d87072817daaf9b9c3dd2580f8
albertmk <albertmk@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-08-22 18:56:42 +0000

    Added more tests for AlgParameter

--- trunk/openmodeller/ctest/om_test_algparameter.cpp
+++ trunk/openmodeller/ctest/om_test_algparameter.cpp
@@ -23,8 +23,44 @@ CxxTest::StaticSuiteDescription suiteDescription_test_AlgParameter( "om_test_alg
 
 static class TestDescription_test_AlgParameter_test1 : public CxxTest::RealTestDescription {
 public:
- TestDescription_test_AlgParameter_test1() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 49, "test1" ) {}
+ TestDescription_test_AlgParameter_test1() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 53, "test1" ) {}
  void runTest() { suite_test_AlgParameter.test1(); }
 } testDescription_test_AlgParameter_test1;
+
+static class TestDescription_test_AlgParameter_test2 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test2() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 59, "test2" ) {}
+ void runTest() { suite_test_AlgParameter.test2(); }
+} testDescription_test_AlgParameter_test2;
+
+static class TestDescription_test_AlgParameter_test3 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test3() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 68, "test3" ) {}
+ void runTest() { suite_test_AlgParameter.test3(); }
+} testDescription_test_AlgParameter_test3;
+
+static class TestDescription_test_AlgParameter_test4 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test4() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 74, "test4" ) {}
+ void runTest() { suite_test_AlgParameter.test4(); }
+} testDescription_test_AlgParameter_test4;
+
+static class TestDescription_test_AlgParameter_test5 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test5() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 81, "test5" ) {}
+ void runTest() { suite_test_AlgParameter.test5(); }
+} testDescription_test_AlgParameter_test5;
+
+static class TestDescription_test_AlgParameter_test6 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test6() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 88, "test6" ) {}
+ void runTest() { suite_test_AlgParameter.test6(); }
+} testDescription_test_AlgParameter_test6;
+
+static class TestDescription_test_AlgParameter_test7 : public CxxTest::RealTestDescription {
+public:
+ TestDescription_test_AlgParameter_test7() : CxxTest::RealTestDescription( Tests_test_AlgParameter, suiteDescription_test_AlgParameter, 95, "test7" ) {}
+ void runTest() { suite_test_AlgParameter.test7(); }
+} testDescription_test_AlgParameter_test7;
 
 #include <cxxtest/Root.cpp>
