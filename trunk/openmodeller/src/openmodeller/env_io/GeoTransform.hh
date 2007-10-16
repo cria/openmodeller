4760e6c4ed7caacf8afeaad8d02efc6d355989b3
timlinux <timlinux@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-10-16 01:12:06 +0000

    Remove scope resolution from default cs var.

--- trunk/openmodeller/src/openmodeller/env_io/GeoTransform.hh
+++ trunk/openmodeller/src/openmodeller/env_io/GeoTransform.hh
@@ -82,7 +82,7 @@ public:
   int transfOut( double *x, double *y, double x0, double y0 ) const;
 
   // Default coordinate system.
-  static char const * GeoTransform::getDefaultCS();
+  static char const * getDefaultCS();
 
 private:
 
