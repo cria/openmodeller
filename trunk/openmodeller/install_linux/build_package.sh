9fefa2acfe515602512abac6d12c86e33a1437d4
timlinux <timlinux@02a2d6a7-111b-0410-992b-806f7cb7853c>
2007-10-23 17:42:10 +0000

    Marked package as 0.5.2 and make script automatically get the install manifest from cmake

--- trunk/openmodeller/install_linux/build_package.sh
+++ trunk/openmodeller/install_linux/build_package.sh
@@ -3,19 +3,27 @@
 if [ -d `pwd`/src ]
 then
   #src exists so we are prolly in the right dir
-  echo "good we are in openModeller checkout dir!"
+  echo "Good we are in openModeller checkout dir!"
 else
   echo "You must run this from the top level openModeller checkout dir!"
   exit 1
 fi
 
 if [ -d `pwd`/debian ]
+then
+  echo "Good we have a debian dir!"
+else
   echo "Debian directory is missing!"
   exit 1
 fi
 
 export DEBFULLNAME="Tim Sutton"
 export DEBEMAIL=tim@linfiniti.com
-dch -v 0.5.1+svn`date +%Y%m%d`
+
+# you may need to run make once to freshen 
+# the manifest before building the package
+cp install_manifest.txt debian/libopenmodeller.txt
+#dch -v 0.5.+svn`date +%Y%m%d`
+dch -v 0.5.2
 fakeroot dpkg-buildpackage
 
