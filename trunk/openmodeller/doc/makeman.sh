8faf2a7bbaea810fe674fb2839793d0da06216cb
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-01-02 19:05:00 +0000

    Created man page for om_pseudo.

--- trunk/openmodeller/doc/makeman.sh
+++ trunk/openmodeller/doc/makeman.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-./txt2man.sh -P om_console -t om_console om_console.txt > om_console.man
+if ! test -d ./man1; then
+  mkdir man1
+fi
+
+./txt2man.sh -P om_console -t om_console om_console.txt > man1/om_console.man
+./txt2man.sh -P om_pseudo -t om_pseudo om_pseudo.txt > man1/om_pseudo.man
 
