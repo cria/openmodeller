20a6b5a1ba23c3beef80630f0a498af36d6b20a4
Renato <1180666+regiov@users.noreply.github.com>
2008-01-02 18:33:23 +0000

    Added txt2man script.

--- doc/makeman.sh
+++ doc/makeman.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-#
-# Note you need the txt2man application installed to use this script!
-#
-# Tim Sutton 2005
-#
-
-txt2man -P om_console -t om_console om_console.txt > om_console.man
+./txt2man.sh -P om_console -t om_console om_console.txt > om_console.man
 
