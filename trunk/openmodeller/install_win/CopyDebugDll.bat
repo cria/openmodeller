ab7dbf7adbad5b7890d51c66334914c6ca54dd51
alexcj <alexcj@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-02-27 11:58:19 +0000

    This bat file copy the necessary dll to the bin directory. This file is used only in MSVC projects.

---
+++ trunk/openmodeller/install_win/CopyDebugDll.bat
@@ -0,0 +1,18 @@
+echo OFF
+if not exist bin\debug mkdir bin\debug
+
+echo Copying gdal15.dll
+copy "%LIB_DIR%\bin\gdal15.dll" bin\debug
+
+echo Copying libexpat.dll
+copy "%LIB_DIR%\bin\libexpat.dll" bin\debug
+
+echo Copying proj.dll
+copy "%LIB_DIR%\bin\proj.dll" bin\debug
+
+REM echo Copying terralib dll's
+REM copy %TERRALIB_HOME%\Debug\terralib_d.dll ..\..\bin\debug
+REM copy %TERRALIB_HOME%\Debug\tiff_d.dll ..\..\bin\debug
+REM copy %TERRALIB_HOME%\Debug\zlib1.dll ..\..\bin\debug
+
+REM PAUSE
