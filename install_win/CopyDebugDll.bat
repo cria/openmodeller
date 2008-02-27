e4f77580b396881b6913c4b5a7e284e01bf1228f
alexcj <alexcj@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-02-27 14:40:33 +0000

    Change the wrong path.

--- install_win/CopyDebugDll.bat
+++ install_win/CopyDebugDll.bat
@@ -1,14 +1,14 @@
 echo OFF
-if not exist bin\debug mkdir bin\debug
+if not exist ..\bin\debug mkdir ..\bin\debug
 
 echo Copying gdal15.dll
-copy "%LIB_DIR%\bin\gdal15.dll" bin\debug
+copy "%LIB_DIR%\bin\gdal15.dll" ..\bin\debug
 
 echo Copying libexpat.dll
-copy "%LIB_DIR%\bin\libexpat.dll" bin\debug
+copy "%LIB_DIR%\bin\libexpat.dll" ..\bin\debug
 
 echo Copying proj.dll
-copy "%LIB_DIR%\bin\proj.dll" bin\debug
+copy "%LIB_DIR%\bin\proj.dll" ..\bin\debug
 
 REM echo Copying terralib dll's
 REM copy %TERRALIB_HOME%\Debug\terralib_d.dll ..\..\bin\debug
