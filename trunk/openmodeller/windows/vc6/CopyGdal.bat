7ffdfb65f7ea5f3b1b0af369c516aecede540488
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-02-27 19:22:43 +0000

    Removed unused directories

--- trunk/openmodeller/windows/vc6/CopyGdal.bat
+++
@@ -1,7 +0,0 @@
-REM  Param1 is the base build directory ('%1')
-
-if not exist ..\%1 mkdir ..\%1
-if not exist ..\%1\lib mkdir ..\%1\lib
-
-echo Copying gdal12.dll to .\%1\lib
-copy %GDAL_HOME%\bin\gdal12.dll ..\%1\lib
--- trunk/openmodeller/windows/vc6/CopyStaticLib.bat
+++
@@ -1,9 +0,0 @@
-REM  Param1 is the base build directory ('%1')
-REM  Param2 is the from directory ('%2')
-REM  Param3 is the output file base name without extension ('%3')
-
-if not exist ..\%1 mkdir ..\%1
-if not exist ..\%1\lib mkdir ..\%1\lib
-
-echo Installing static library %2\%3.lib and additional files to .\%1\lib
-copy %2\%3.lib  ..\%1\lib
