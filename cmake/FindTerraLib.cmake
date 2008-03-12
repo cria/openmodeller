40de2a520ca3e6d838917c7748c961b1691ecefc
alexcj <alexcj@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-03-12 18:22:02 +0000

    Look for TerraLib includes and lib.

---
+++ cmake/FindTerraLib.cmake
@@ -0,0 +1,40 @@
+# CMake module to search for TERRALIB library
+#
+# If it's found it sets TERRALIB_FOUND to TRUE
+# and following variables are set:
+#    TERRALIB_INCLUDE_DIR
+#    TERRALIB_LIBRARY
+
+
+FIND_PATH(TERRALIB_INCLUDE_DIR TeRaster.h
+  /usr/local/include 
+  /usr/include 
+  "$ENV{LIB_DIR}/include/terralib"
+  c:/msys/local/include
+  )
+  
+  FIND_LIBRARY(TERRALIB_LIBRARY NAMES terralib PATHS 
+  /usr/local/lib 
+  /usr/lib 
+  "$ENV{LIB_DIR}/lib"
+  c:/msys/local/lib
+  )
+  
+IF (TERRALIB_INCLUDE_DIR AND TERRALIB_LIBRARY)
+   SET(TERRALIB_FOUND TRUE)
+ENDIF (TERRALIB_INCLUDE_DIR AND TERRALIB_LIBRARY)
+
+
+IF (TERRALIB_FOUND)
+
+   IF (NOT TERRALIB_FIND_QUIETLY)
+      MESSAGE(STATUS "Found TERRALIB: ${TERRALIB_LIBRARY}")
+   ENDIF (NOT TERRALIB_FIND_QUIETLY)
+
+ELSE (TERRALIB_FOUND)
+
+   IF (TERRALIB_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find TERRALIB")
+   ENDIF (TERRALIB_FIND_REQUIRED)
+
+ENDIF (TERRALIB_FOUND)
