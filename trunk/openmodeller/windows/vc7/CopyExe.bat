c3cac75d65f7166159c945010edaceeaf5b9fef4
alexcj <alexcj@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-03-12 18:33:18 +0000

    Remove old VC7 build files.

--- trunk/openmodeller/windows/vc7/CopyExe.bat
+++
@@ -1,8 +0,0 @@
-REM  Param1 is the base build directory ('%1')
-REM  Param2 is the from directory ('%2')
-REM  Param3 is the output file base name without extension ('%3')
-
-if not exist ..\%1 mkdir ..\%1
-
-echo Installing executable %2\%3.exe to .\%1
-copy %2\%3.exe  ..\%1
