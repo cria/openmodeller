af6de6d1f368bb5f94f5444ba1548051aabcc2a4
elscr <elscr@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-03-13 13:08:55 +0000

    Initial maxent implementation

---
+++ trunk/openmodeller/src/algorithms/maxent/maximum_entropy.cpp
@@ -0,0 +1,284 @@
+#include "maximum_entropy.hh"
+#include <openmodeller/MeanVarianceNormalizer.hh>
+#include <openmodeller/Sampler.hh>
+#include "maxent.h"
+#include <iostream>
+
+using namespace std;
+
+/****************************************************************/
+/********************** Algorithm's Metadata ********************/
+
+#define NUM_PARAM 0
+
+/******************************/
+/*** Algorithm's parameters ***/
+
+  static AlgParamMetadata *parameters = 0;
+
+/************************************/
+/*** Algorithm's general metadata ***/
+
+static AlgMetadata metadata = {
+  
+  "MaximumEntropy", 	// Id.
+  "Maximum Entropy",    // Name.
+  "0.1",       	        // Version.
+
+  // Overview.
+  "",
+
+  // Description.
+  "",
+
+  "", // Algorithm author.
+
+  "", // Bibliography.
+
+  "Elisangela S. da C. Rodrigues", // Code author.
+
+  "elisangela.rodrigues [at] poli.usp.br", // Code author's contact.
+
+  0, // Does not accept categorical data.
+  0, // Does not need (pseudo)absence points.
+
+  NUM_PARAM, // Algorithm's parameters.
+  parameters
+};
+
+/****************************************************************/
+/****************** Algorithm's factory function ****************/
+
+OM_ALG_DLL_EXPORT
+AlgorithmImpl *
+algorithmFactory()
+{
+  return new MaximumEntropy();
+}
+
+OM_ALG_DLL_EXPORT
+AlgMetadata const *
+algorithmMetadata()
+{
+  return &metadata;
+}
+
+/****************************************************************/
+/************************ Maximum Entropy ***********************/
+
+/*******************/
+/*** constructor ***/
+
+MaximumEntropy::MaximumEntropy() :
+  AlgorithmImpl(&metadata),
+  _done(false),
+  num_layers(0)
+{ 
+  _normalizerPtr = new MeanVarianceNormalizer();
+}
+
+/******************/
+/*** destructor ***/
+
+MaximumEntropy::~MaximumEntropy()
+{
+}
+
+/******************/
+/*** initialize ***/
+
+int
+MaximumEntropy::initialize()
+{
+  // Check the number of presences.
+  int num_presences = _samp->numPresence();
+
+  //Debug
+  //cout << "Numero de pontos de presenca " << num_presences << endl;
+
+  if (num_presences == 0){
+
+    Log::instance()->error("MaximumEntropy: No presence points inside the mask!\n");
+    return 0;
+  }
+  
+  // Load presence points.
+  presences = _samp->getPresences();
+
+  // Check the number of absences.
+  int num_absences = _samp->numAbsence();
+
+  //Debug
+  //cout << "Numero de pontos de ausencia " << num_absences << endl;
+
+  if (num_absences == 0){
+
+    num_absences = num_presences;
+
+    Log::instance()->info("Generating pseudo-absences.\n");
+
+    OccurrencesImpl * occs = new OccurrencesImpl("", GeoTransform::getDefaultCS());
+
+    absences = ReferenceCountedPointer<OccurrencesImpl>(occs);
+
+    for (int i = 0; i < num_absences; ++i){
+
+      OccurrencePtr oc = _samp->getPseudoAbsence();
+
+      absences->insert(oc);
+    }
+    
+    // Compute normalization with all points
+    SamplerPtr mySamplerPtr = createSampler(_samp->getEnvironment(), presences, absences);
+
+      _normalizerPtr->computeNormalization(mySamplerPtr);
+
+      setNormalization(_samp);
+
+      absences->normalize(_normalizerPtr);
+  }
+  else{
+    absences = _samp->getAbsences();
+  }
+  return 1;
+}
+
+/***************/
+/*** iterate ***/
+
+int
+MaximumEntropy::iterate()
+{
+  
+  add_samples(model);
+
+  model.train();
+
+  //Debug
+  cout << "Saving the model... " << endl;
+
+  //model.save_to_file("model"); //save the model into a file
+  
+  _done = true;
+
+  return 1;
+}
+
+/************/
+/*** done ***/
+
+int
+MaximumEntropy::done() const
+{
+  return _done;
+}
+
+/***********************/
+/*** Train the Model ***/
+
+void 
+MaximumEntropy::add_samples(ME_Model & model)
+{
+  // Number of layers.
+  num_layers = _samp->numIndependent();
+
+  //Debug
+  //cout << "Numero de camadas " << num_layers << endl;
+
+  OccurrencesImpl::const_iterator p_iterator;
+  OccurrencesImpl::const_iterator p_end;
+
+  p_iterator = absences->begin();
+  p_end = absences->end();
+
+  //int j=1;//debug
+
+  while (p_iterator != p_end){
+
+    Sample point = (*p_iterator)->environment();
+    
+    ME_Sample s("0");
+
+    for (int i = 0; i < num_layers; ++i)
+      s.add_feature("0", point[i]);
+
+    //Debug
+    //cout << "Absence point " << j << " " << point << endl;
+    //j++;
+
+    model.add_training_sample(s);
+    
+    ++p_iterator;
+  }
+
+  p_iterator = presences->begin();
+  p_end = presences->end();
+
+  //j=1;//debug
+
+  while (p_iterator != p_end){
+
+    Sample point = (*p_iterator)->environment();
+
+    ME_Sample s("1");    
+    
+    for (int i = 0; i < num_layers; ++i)
+      s.add_feature("1", point[0]);
+
+    //Debug
+    //cout << "Presence point " << j << " " << point << endl;
+    //j++;
+    
+    model.add_training_sample(s);
+    
+    ++p_iterator;
+  }
+
+}
+
+/*****************/
+/*** get Value ***/
+
+Scalar
+MaximumEntropy::getValue(const Sample& x) const
+{
+  //vector<double> vp = model.classify(x);
+  return 1.0;
+}
+
+/***********************/
+/*** get Convergence ***/
+/*
+int
+MaximumEntropy::getConvergence(Scalar *val)
+{
+  *val = 1.0;
+  return 1;
+}
+*/
+
+/****************************************************************/
+/****************** configuration *******************************/
+
+void
+MaximumEntropy::_getConfiguration(ConfigurationPtr& config) const
+{
+  if (!_done)
+    return;
+  
+  ConfigurationPtr model_config(new ConfigurationImpl("MaximumEntropy"));
+  config->addSubsection(model_config);
+
+  //////////////////COMPLETAR////////////////////
+}
+
+void
+MaximumEntropy::_setConfiguration(const ConstConfigurationPtr& config)
+{
+  ConstConfigurationPtr model_config = config->getSubsection("MaximumEntropy",false );
+
+  if (!model_config)
+    return;
+  
+  _done = true;
+}
---
+++ trunk/openmodeller/src/algorithms/maxent/maximum_entropy.hh
@@ -0,0 +1,75 @@
+/**
+ * Declaration of Maximum Entropy algorithm class.
+ * 
+ * @author Elisangela S. da C. Rodrigues (elisangela.rodrigues [at] poli.usp.br)
+ * @date   2008-02-24
+ * $Id: maximum_entropy.hh $//////////////////////////COMPLETAR////////////////
+ * 
+ * LICENSE INFORMATION 
+ * 
+ * Copyright(c) ///////////////////////COMPLETAR///////////////////////
+ *
+ * http://www.poli.usp.br ////////////////////VER///////////////////
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef _MAXIMUM_ENTROPYHH_
+#define _MAXIMUM_ENTROPYHH_
+
+#include <openmodeller/om.hh>
+
+#include "maxent.h"
+
+/*********************************************************/
+/******************** Maximum Entropy ********************/
+
+/*
+ * Algorithm to generate models based on Maximum Entropy.
+ */
+class MaximumEntropy : public AlgorithmImpl
+{
+public:
+
+  MaximumEntropy();
+
+  ~MaximumEntropy();
+
+  int initialize();
+  int iterate();
+  int done() const;
+
+  void add_samples(ME_Model & model);
+
+  Scalar getValue( const Sample& x ) const;
+
+  //int getConvergence( Scalar *val );
+
+protected:
+
+  virtual void _getConfiguration( ConfigurationPtr& ) const;
+
+  virtual void _setConfiguration( const ConstConfigurationPtr& );
+
+  bool _done;
+
+  int num_layers;
+
+  ME_Model model;
+
+  OccurrencesPtr absences;
+  OccurrencesPtr presences;
+};
+
+#endif
+
