fafdc90fac4087ed4d192e5939bd10572618b388
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-03-25 19:59:35 +0000

    Added man page for om_create.

--- trunk/openmodeller/doc/makeman.sh
+++ trunk/openmodeller/doc/makeman.sh
@@ -2,5 +2,6 @@
 
 # Console documentation
 ./txt2man.sh -P om_console -t om_console om_console.txt > ../src/console/man/om_console.1
+./txt2man.sh -P om_create -t om_create om_create.txt > ../src/console/man/om_create.1
 ./txt2man.sh -P om_pseudo -t om_pseudo om_pseudo.txt > ../src/console/man/om_pseudo.1
 
