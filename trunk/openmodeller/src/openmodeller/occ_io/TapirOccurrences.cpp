a6b8f75911d500ed6ed239e4a9bfc935f1ff0c89
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2008-06-18 20:35:35 +0000

    Replaced TAPIR search template by a another one with a more restrictive filter.

--- trunk/openmodeller/src/openmodeller/occ_io/TapirOccurrences.cpp
+++ trunk/openmodeller/src/openmodeller/occ_io/TapirOccurrences.cpp
@@ -493,7 +493,7 @@ TapirOccurrences::_retrieveRecords( TapirRecordData *data, int limit )
 
   search_url << "&sciname=" << curl_easy_escape( curl_handle, data->_occurrences->name(), 0 );
 
-  search_url << "&t=" << curl_easy_escape( curl_handle, "http://openmodeller.cria.org.br/xml/tapir/1.0/st.xml", 0 );
+  search_url << "&t=" << curl_easy_escape( curl_handle, "http://openmodeller.cria.org.br/xml/tapir/1.0/st_v2.xml", 0 );
 
   // After using next to make the URL, set it to -1 to stop the process in case 
   // the response does not return the "next" attribute
