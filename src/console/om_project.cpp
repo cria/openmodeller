4203d6e2f52208d78335f41cf2abfcfca00ccb1e
Renato <1180666+regiov@users.noreply.github.com>
2008-10-03 21:59:16 +0000

    Added parameters --model, --template and --format to om_project to facilitate native projections for users.

--- src/console/om_project.cpp
+++ src/console/om_project.cpp
@@ -41,6 +41,9 @@ int main( int argc, char **argv ) {
   // command-line parameters (short name, long name, description, take args)
   opts.addOption( "v", "version"  , "Display version info"                        , false );
   opts.addOption( "r", "xml-req"  , "Projection request file in XML"              , true );
+  opts.addOption( "o", "model"    , "File with serialized model (native projection)", true );
+  opts.addOption( "t", "template" , "Raster template for the distribution map (native projection)", true );
+  opts.addOption( "f", "format"   , "File format for the distribution map (native projection)", true );
   opts.addOption( "m", "dist-map" , "File to store the generated model"           , true );
   opts.addOption( "" , "log-level", "Set the log level (debug, warn, info, error)", true );
   opts.addOption( "" , "log-file" , "Log file"                                    , true );
@@ -49,6 +52,9 @@ int main( int argc, char **argv ) {
 
   std::string log_level("info");
   std::string request_file;
+  std::string model_file;
+  std::string tmpl_file;
+  std::string format;
   std::string map_file;
   std::string log_file;
   std::string progress_file;
@@ -65,7 +71,7 @@ int main( int argc, char **argv ) {
     switch ( option ) {
 
       case 0:
-        printf("om_project 0.2.2\n");
+        printf("om_project 0.3\n");
         printf("This is free software; see the source for copying conditions. There is NO\n");
         printf("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
         exit(0);
@@ -74,18 +80,27 @@ int main( int argc, char **argv ) {
         request_file = opts.getArgs( option );
         break;
       case 2:
-        map_file = opts.getArgs( option );
+        model_file = opts.getArgs( option );
         break;
       case 3:
-        log_level = opts.getArgs( option );
+        tmpl_file = opts.getArgs( option );
         break;
       case 4:
-        log_file = opts.getArgs( option );
+        format = opts.getArgs( option );
         break;
       case 5:
-        progress_file = opts.getArgs( option );
+        map_file = opts.getArgs( option );
+        break;
+      case 6:
+        log_level = opts.getArgs( option );
+        break;
+      case 7:
+        log_file = opts.getArgs( option );
         break;
       case 8:
+        progress_file = opts.getArgs( option );
+        break;
+      case 9:
         statistics_file = opts.getArgs( option );
         break;
       default:
@@ -93,12 +108,31 @@ int main( int argc, char **argv ) {
     }
   }
 
+  // Log stuff
+
+  Log::Level level_code = getLogLevel( log_level );
+
+  if ( ! log_file.empty() ) {
+
+    Log::instance()->set( level_code, log_file, "" );
+  }
+  else {
+ 
+    // Just set the level - things will go to stderr
+    Log::instance()->setLevel( level_code );
+  }
+
   // Check parameters
 
-  if ( request_file.empty() ) {
+  if ( request_file.empty() && model_file.empty() ) {
 
-    printf( "Please specify a projection request file in XML\n");
+    printf( "Please specify either a projection request file in XML or a serialized model\n");
     exit(-1);
+  }
+
+  if ( (! request_file.empty()) && ( (! model_file.empty()) || (! tmpl_file.empty()) || (! format.empty() ) ) ) {
+
+    Log::instance()->warn( "When a request file is specified, --model, --template and --format are ignored" );
   }
 
   if ( map_file.empty() ) {
@@ -120,20 +154,6 @@ int main( int argc, char **argv ) {
 
     // Always create initial file with status "queued" (-1)
     progressFileCallback( -1.0, &prog_data );
-  }
-
-  // Log stuff
-
-  Log::Level level_code = getLogLevel( log_level );
-
-  if ( ! log_file.empty() ) {
-
-    Log::instance()->set( level_code, log_file, "" );
-  }
-  else {
- 
-    // Just set the level - things will go to stderr
-    Log::instance()->setLevel( level_code );
   }
 
   // Real work
@@ -160,10 +180,42 @@ int main( int argc, char **argv ) {
 
     std::ostringstream model_output;
 
-    ConfigurationPtr input = Configuration::readXml( request_file.c_str() );
-    om.setProjectionConfiguration( input );
+    if ( ! request_file.empty() ) {
 
-    om.createMap( map_file.c_str() );
+      ConfigurationPtr input = Configuration::readXml( request_file.c_str() );
+      om.setProjectionConfiguration( input );
+      om.createMap( map_file.c_str() );
+    }
+    else {
+
+      // Native projection - get original environment from serialized model
+      ConfigurationPtr input = Configuration::readXml( model_file.c_str() );
+
+      om.setModelConfiguration( input );
+
+      EnvironmentPtr env = om.getEnvironment();
+
+      MapFormat tmpl;
+
+      if ( tmpl_file.empty() ) {
+
+        // Use first layer as reference
+        std::string first_layer = env->getLayerPath(0);
+
+        tmpl = MapFormat( first_layer.c_str() );
+      }
+      else {
+
+        tmpl = MapFormat( tmpl_file.c_str() );
+      }
+
+      if ( ! format.empty() ) {
+
+        tmpl.setFormat( format );
+      }
+
+      om.createMap( env, map_file.c_str(), tmpl );
+    }
 
     AreaStats * stats = om.getActualAreaStats();
 
