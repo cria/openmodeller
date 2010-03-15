d27848903088ee73312ba23e94183fb687f6eeb3
Renato <1180666+regiov@users.noreply.github.com>
2010-03-15 22:42:41 +0000

    Updated alg version number.

--- src/algorithms/maxent/maximum_entropy.cpp
+++ src/algorithms/maxent/maximum_entropy.cpp
@@ -133,7 +133,7 @@ static AlgMetadata metadata = {
   
   "MAXENT",          // Id.
   "Maximum Entropy", // Name.
-  "0.2",       	     // Version.
+  "0.3",       	     // Version.
 
   // Overview.
   "The principle of maximum entropy is a method for analyzing available qualitative information in order to determine a unique epistemic probability distribution. It states that the least biased distribution that encodes certain given information is that which maximizes the information entropy (content retrieved from Wikipedia on the 19th of May, 2008: http://en.wikipedia.org/wiki/Maximum_entropy).",
