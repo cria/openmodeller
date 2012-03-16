17d1d602bdcac81a9c22b57f4ac6e2799acf9fee
dbolgheroni <dbolgheroni@02a2d6a7-111b-0410-992b-806f7cb7853c>
2012-03-16 12:04:18 +0000

    Change the way we're dealing with layers update. Still needs testing as it is.

--- trunk/openmodeller/src/soap/synclayers.sh
+++ trunk/openmodeller/src/soap/synclayers.sh
@@ -56,55 +56,66 @@ fi
 # 1. a long running job
 # 2. a fast job
 
-# Check to see if there are changes in repository
-# TODO: Check the status of rsync, in the case server is down
-if [ ! -e REPOSITORY_CHANGED ]; then
-    # check the status of rsync, to certify that server is running
-    rsync -ani --delete --no-motd rsync://$RSYNC_LAYERS_REPOSITORY \
-        $LAYERS_DIRECTORY > rsync.out.tmp
-    $SERVER_STATUS=$?
+# by default, we'll use 2 repositories in each server, at least when
+# this script syncs, which are (1) the effective repository, which is
+# $LAYERS_DIRECTORY and (2) the new repository, which is
+# $UPDATED_LAYERS_DIRECTORY
+#
+# Status codes:
+# 0: NORMAL_STAND_BY
+# 1: RUNNING (rsync initiated)
+# 2: READY
+# 3: ABORTED
 
-    grep -E '^(\*|>)' rsync.out.tmp
-    $FOUND_CHANGES=$?
-
-    if [ $SERVER_STATUS -a $FOUND_CHANGES ]; then
-    # Change the status of the server accordingly
+check_om_processes() {
+    # oM instances running?
+    if [ $(pgrep om_console) ]; then
+        exit 1
+    else
+        # lock server
         cat server.conf | \
-            sed 's/SYSTEM_STATUS=1/SYSTEM_STATUS=2/' > server.conf.tmp
+            sed 's/SYSTEM_STATUS=1/SYSTEM_STATUS=2/' > \
+            server.conf.tmp
         mv server.conf.tmp server.conf
 
-        touch REPOSITORY_CHANGED
-    else
-        # there server is down or there are no changes
+        # swap directories
+        rm -fr $LAYERS_DIRECTORY/
+        mv $UPDATED_LAYERS_DIRECTORY/ $LAYERS_DIRECTORY/
+        mkdir $UPDATED_LAYERS_DIRECTORY/
+
+        # remove flag
+        rm $STATUS_DIR_PREFIX/COPY_READY
+    fi
+}
+
+if [ $(pgrep rsync) ]; then
+    exit 1
+else
+    # check flag
+    if [ -e $STATUS_DIR_PREFIX/COPY_READY ]; then
+        check_om_processes
+
         exit 0
+    else
+        rsync -ani --delete --no-motd rsync://$RSYNC_LAYERS_DIRECTORY \
+            $LAYERS_DIRECTORY > rsync.out.tmp
+        $RSYNC_LAYERS_REPOSITORY_STATUS=$?
+
+        grep -E '^(\*|>)' rsync.out.tmp
+        $HAVE_UPDATES=$?
+
+        if [ $RSYNC_LAYERS_REPOSITORY_STATUS -and $HAVE_UPDATES ]; then
+            rsync -a --delete --no-motd rsync://$RSYNC_LAYERS_DIRECTORY \
+                $LAYERS_DIRECTORY
+
+            $RSYNC_LAYERS_REPOSITORY_STATUS=$?
+            if [ $RSYNC_LAYERS_DIRECTORY_STATUS ]; then
+                touch $STATUS_DIRECTORY_PREFIX/COPY_READY
+
+                check_om_processes
+            else
+                exit 0
+            fi
+        fi
     fi
 fi
-
-# Wait for all jobs to finish (check number of files under $PID_DIRECTORY)
-#
-# Two problems arise here:
-# 1. If we check for files under $PID_DIRECTORY, the script will exit
-# early and a new request to the $RSYNC_LAYERS_REPOSITORY will be
-# generated next time cron runs this 
-#
-# 2. If we wait here long enough, cron will be called _before_ we leave
-# this script, so the correct time must be set
-# 
-# An alternative is to use a local file to bypass the rsync check if it
-# was already checked that the repository has changed, which is what we
-# do before the first rsync.
-
-if [ ]; then
-    # TODO: Check the format of the files under $PID_DIRECTORY
-else
-    # Synchronize layers
-    rsync -a --delete --no-motd rsync://$RSYNC_LAYERS_REPOSITORY $LAYERS_DIRECTORY
-    rm REPOSITORY_CHANGED
-
-    # Switch back system status to 1 (normal)
-    cat server.conf | \
-        sed 's/SYSTEM_STATUS=2/SYSTEM_STATUS=1/' > server.conf.tmp
-    mv server.conf.tmp server.conf
-fi
-
-# TODO: Remove getLayers cached response (layers.xml under $CACHE_DIRECTORY)
