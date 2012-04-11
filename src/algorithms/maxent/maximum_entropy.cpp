3d8708947e44a8cee771628ebbbcd276de36a77e
Renato <1180666+regiov@users.noreply.github.com>
2012-04-11 20:04:57 +0000

    Deallocate generators.

--- src/algorithms/maxent/maximum_entropy.cpp
+++ src/algorithms/maxent/maximum_entropy.cpp
@@ -299,6 +299,13 @@ MaximumEntropy::~MaximumEntropy()
 
   for (unsigned int i = 0; i < n; ++i)
     delete _features[i];
+
+  unsigned int m = _generators.size();
+
+  Log::instance()->debug("Deallocating %d generators\n", n);
+
+  for (unsigned int j = 0; j < m; ++j)
+    delete _generators[j];
 }
 
 /**************************/
