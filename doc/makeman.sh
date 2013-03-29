3d703bc5336fbae58bf303968e55ad030be16db0
Renato <1180666+regiov@users.noreply.github.com>
2013-03-29 20:33:02 +0000

    Included documentation for om_evaluate

--- doc/makeman.sh
+++ doc/makeman.sh
@@ -9,4 +9,4 @@
 ./txt2man.sh -P om_test -t om_test om_test.txt > ../src/console/man/om_test.1
 ./txt2man.sh -P om_project -t om_project om_project.txt > ../src/console/man/om_project.1
 ./txt2man.sh -P om_niche -t om_niche om_niche.txt > ../src/console/man/om_niche.1
-
+./txt2man.sh -P om_evaluate -t om_evaluate om_evaluate.txt > ../src/console/man/om_evaluate.1
