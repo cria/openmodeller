87d73bf03e32e7126468d075a4816f69e614aa54
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2013-07-23 14:12:18 +0000

    Moved helper functions to a separate file

---
+++ trunk/openmodeller/src/soap/2.0/omws_utils.cpp
@@ -0,0 +1,251 @@
+/**
+ * openModeller Web service utils library.
+ * 
+ * @author Renato De Giovanni (renato [at] cria [dot] org [dot] br)
+ * $Id: $
+ * 
+ * LICENSE INFORMATION 
+ * 
+ * Copyright(c) 2013 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include "omH.h"
+
+#include <openmodeller/FileParser.hh>
+
+#include <fstream>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+#include "omws_utils.hh"
+
+/********************/
+/**** fileExists ****/
+bool 
+fileExists( const char* fileName )
+{ 
+  FILE * file = fopen( fileName, "r" );
+
+  if ( file != NULL ) {
+
+    fclose( file );
+
+    return true;
+  }
+
+  return false;
+}
+
+/*********************/
+/**** getProgress ****/
+int 
+getProgress( const string & progFileName )
+{
+  int progress = -1;
+
+  if ( fileExists( progFileName.c_str() ) ) {
+
+    // If file exists, get its content
+    fstream fin;
+    fin.open( progFileName.c_str(), ios::in );
+
+    if ( fin.is_open() ) {
+
+      // If file was opened, read the content
+      ostringstream oss;
+      string line;
+
+      getline( fin, line );
+      oss << line << endl;
+
+      // Note: if the content is empty, atoi will return 0
+      progress = atoi( oss.str().c_str() );
+
+      fin.close();
+    }
+    else {
+
+      // This should never happen. Report job abortion.
+      progress = -2;
+    }
+  }
+
+  return progress;
+}
+
+/************************/
+/**** updateProgress ****/
+bool 
+updateProgress( const string & progFileName, const char * content )
+{
+  FILE *fd;
+
+  if ( fileExists( progFileName.c_str() ) ) {
+
+    fd = fopen( progFileName.c_str(), "w" );
+  }
+  else {
+
+    fd = fopen( progFileName.c_str(), "a" );
+  }
+
+  if ( fd == NULL ) {
+
+    printf("Could not create/open progress file %s\n", progFileName.c_str());
+    return false;
+  }
+
+  if ( fputs( content, fd ) < 0 ) {
+
+    printf("Could not write to progress file %s\n", progFileName.c_str());
+    return false;
+  }
+
+  fclose( fd );
+
+  return true;
+}
+
+/********************/
+/**** getTickets ****/
+vector<string>
+getTickets( const string & concatenatedValues )
+{
+  vector<string> tickets;
+
+  // Split tokens and process each one
+  stringstream ss( concatenatedValues );
+  string ticket;
+  while ( getline( ss, ticket, ',' ) ) {
+
+    tickets.push_back( ticket );
+  }
+
+  return tickets;
+}
+
+
+/********************/
+/**** getTickets ****/
+map<string, string>
+getTicketsWithTask( const string & concatenatedValues )
+{
+  map<string, string> tickets;
+
+  // Split tokens and process each one
+  stringstream ss( concatenatedValues );
+  string task_ticket;
+  while ( getline( ss, task_ticket, ',' ) ) {
+
+    size_t pos = task_ticket.find( "_" );
+
+    if ( pos != string::npos ) {
+
+      string task = task_ticket.substr( 0, pos );
+      string ticket = task_ticket.substr( pos+1, 6 );
+
+      tickets.insert( pair< string, string >( ticket, task ) );
+    }
+  }
+
+  return tickets;
+}
+
+/********************/
+/**** createFile ****/
+bool 
+createFile( const string & fileName )
+{
+  FILE *fd = fopen( fileName.c_str(), "w" );
+
+  if ( fd == NULL ) {
+
+    printf("Could not create file %s\n", fileName.c_str());
+    return false;
+  }
+
+  fclose( fd );
+
+  return true;
+}
+
+
+/**************************/
+/**** cancelExperiment ****/
+void
+cancelExperiment( const string & exp_metadata_file, const string & exp_prog_file, const string & exp_done_file, const string & ticket_dir, const string & job_ticket, FILE * fd_log )
+{
+  fputs( "Cancelling experiment\n", fd_log );
+
+  // Are there other jobs to cancel?
+
+  // Get all jobs in the experiment
+  FileParser exp_data( exp_metadata_file );
+
+  string all_jobs = exp_data.get( "JOBS" );
+
+  vector<string> all_tickets = getTickets( all_jobs );
+
+  for ( vector<string>::iterator at = all_tickets.begin(); at != all_tickets.end(); ++at ) {
+
+    if ( job_ticket.compare(0, 6, (*at)) == 0 ) {
+
+      continue; // Skip current job!
+    }
+
+    // Read job metadata to get type
+    string metadata_file = ticket_dir + OMWS_JOB_METADATA_PREFIX + (*at);
+
+    FileParser data( metadata_file );
+
+    string type = data.get("TYPE");
+
+    // Build pending request file name
+    string pending_file = ticket_dir + type + _PENDING_REQUEST + (*at);
+
+    // If job is pending, cancel it, otherwise let it finish behind the scenes
+    if ( fileExists( pending_file.c_str() ) ) {
+
+      string prog_file = ticket_dir + OMWS_JOB_PROGRESS_PREFIX + (*at);
+
+      updateProgress( prog_file, "-3" );
+      string done_file = ticket_dir + OMWS_JOB_DONE_PREFIX + (*at);
+      createFile( done_file );
+    }
+  }
+
+  // Cancel experiment
+  updateProgress( exp_prog_file, "-2" );
+  createFile( exp_done_file );
+}
+
+
+/*********************/
+/**** releaseSoap ****/
+void 
+releaseSoap(struct soap *ctx1, struct soap *ctx2)
+{
+  soap_destroy(ctx1); // remove deserialized class instances (C++ objects)
+  soap_end(ctx1);     // clean up and remove deserialized data
+  soap_done(ctx1);    // detach context (last use and no longer in scope)
+
+  soap_destroy(ctx2);
+  soap_end(ctx2);
+  soap_done(ctx2);
+}
---
+++ trunk/openmodeller/src/soap/2.0/omws_utils.hh
@@ -0,0 +1,64 @@
+/**
+ * openModeller Web service utils library.
+ * 
+ * @author Renato De Giovanni (renato [at] cria [dot] org [dot] br)
+ * $Id: $
+ * 
+ * LICENSE INFORMATION 
+ * 
+ * Copyright(c) 2013 by CRIA -
+ * Centro de Referencia em Informacao Ambiental
+ *
+ * http://www.cria.org.br
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details:
+ * 
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef OMWS_UTILS_H
+#define OMWS_UTILS_H
+
+#include <string.h>
+#include <map>
+using namespace std;
+
+#define OMWS_MODEL "model"
+#define OMWS_TEST "test"
+#define OMWS_PROJECTION "proj"
+#define OMWS_EVALUATE "eval"
+#define OMWS_SAMPLING "samp"
+#define OMWS_EXPERIMENT "exp"
+#define _REQUEST "_req."
+#define _PROCESSED_REQUEST "_proc."
+#define _RESPONSE "_resp."
+#define _PENDING_REQUEST "_pend."
+#define OMWS_JOB_PROGRESS_PREFIX "prog."
+#define OMWS_JOB_DONE_PREFIX "done."
+#define OMWS_JOB_METADATA_PREFIX "job."
+
+bool fileExists( const char* fileName );
+
+int getProgress( const string & progFileName );
+
+bool updateProgress( const string & progFileName, const char * content );
+
+vector<string> getTickets( const string & concatenatedValues );
+
+map<string, string> getTicketsWithTask( const string & concatenatedValues );
+
+bool createFile( const string & fileName );
+
+void cancelExperiment( const string & exp_metadata_file, const string & exp_prog_file, const string & exp_done_file, const string & ticket_dir, const string & job_ticket, FILE * fd_log );
+
+void releaseSoap(struct soap *ctx1, struct soap *ctx2);
+
+#endif
