660e646262ec26c0fa4f2c710b4f2ebdb2b1ffe0
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2013-08-13 19:14:28 +0000

    Started revising the new web site

--- trunk/openmodeller/doc/site/build.sh
+++ trunk/openmodeller/doc/site/build.sh
@@ -6,7 +6,9 @@ echo "===> generating HTML pages directly from openModeller"
 
 # generate pages
 echo "===> generating HTML pages from .t2t files"
-files="developers 
+files="INSTALL
+       alg_manual
+       developers 
        documentation 
        environmental_data 
        main
@@ -20,4 +22,9 @@ files="developers
 for f in $files; do
     txt2tags $f.t2t
 done
+
+txt2tags -o web_service_1-0.html ../../src/soap/1.0/web_service.t2t
+txt2tags -o om_server_1-0.html ../../src/soap/1.0/om_server.t2t
+txt2tags -o web_service_2-0.html ../../src/soap/2.0/web_service.t2t
+txt2tags -o om_server_2-0.html ../../src/soap/2.0/om_server.t2t
 
