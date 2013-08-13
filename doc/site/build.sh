a25376a7dfaf941908bd471bcb621f01e4cfa011
Renato <1180666+regiov@users.noreply.github.com>
2013-08-13 19:17:48 +0000

    Returned installation guide to the main documentation directory

--- doc/site/build.sh
+++ doc/site/build.sh
@@ -6,8 +6,7 @@ echo "===> generating HTML pages directly from openModeller"
 
 # generate pages
 echo "===> generating HTML pages from .t2t files"
-files="INSTALL
-       alg_manual
+files="alg_manual
        developers 
        documentation 
        environmental_data 
@@ -23,6 +22,7 @@ for f in $files; do
     txt2tags $f.t2t
 done
 
+txt2tags -o INSTALL.html ../INSTALL.t2t
 txt2tags -o web_service_1-0.html ../../src/soap/1.0/web_service.t2t
 txt2tags -o om_server_1-0.html ../../src/soap/1.0/om_server.t2t
 txt2tags -o web_service_2-0.html ../../src/soap/2.0/web_service.t2t
