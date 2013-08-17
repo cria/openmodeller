1d489db67933fd3d2a9b0714eb4b65b939ef46a8
rdg <rdg@02a2d6a7-111b-0410-992b-806f7cb7853c>
2013-08-17 20:44:42 +0000

    Included change log to the new web site

--- trunk/openmodeller/doc/site/build.sh
+++ trunk/openmodeller/doc/site/build.sh
@@ -21,7 +21,7 @@ sed -i 's/<\/HEAD>/<LINK REL="stylesheet" TYPE="text\/css" HREF="..\/om.css"><\/
 sed -i 's/<A HREF="\/cgi-bin\/man\/man2html">man2html<\/A>/man2html/g' man1/*.1.html
 sed -i 's/<A HREF="..\/index.html">Return to Main Contents<\/A>//g' man1/*.1.html
 
-# generate pages
+# generate regular t2t pages
 echo "===> generating HTML pages from .t2t files"
 files="alg_manual
        developers 
@@ -36,11 +36,20 @@ for f in $files; do
     txt2tags $f.t2t
 done
 
+# Installation guide
 cp -u ../INSTALL.t2t ./INSTALL.t2t
 txt2tags -o INSTALL.html INSTALL.t2t
 
+# Web service documentation
 txt2tags -o web_service.html ../../src/soap/1.0/web_service.t2t
 txt2tags -o om_server.html ../../src/soap/1.0/om_server.t2t
 txt2tags -o web_service_2.html ../../src/soap/2.0/web_service.t2t
 txt2tags -o om_server_2.html ../../src/soap/2.0/om_server.t2t
 
+# Change log
+cp -u ../../ChangeLog ChangeLog
+echo "\n\n\n%!target: html\n%! style: om.css\n%! encoding: utf-8\n%!include: menu.t2t" > changelog.t2t
+cat ChangeLog >> changelog.t2t
+sed -i 's/\t\*/-/g' changelog.t2t
+sed -i 's/^\(Release.*\)/**\1**/g' changelog.t2t
+txt2tags -o changelog.html changelog.t2t
